03e75cf783e8c8d661f1465f8e616848132cb832
Pedro Pombeiro <noreply@pedro.pombei.ro>
2020-04-04 21:54:58 +0200

    Add support for upgrading custom Oh-my-zsh plugins and themes

--- .install/linux/upgrade.sh
+++ .install/linux/upgrade.sh
@@ -4,5 +4,10 @@
 sudo apt update
 sudo apt upgrade
 
+# Update Oh-my-zsh custom themes and plugins
+git -C ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k pull
+git -C ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions pull
+git -C ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting pull
+
 # Update npm packages
 npm update -g
--- .install/osx/upgrade.sh
+++ .install/osx/upgrade.sh
@@ -7,5 +7,10 @@ sudo softwareupdate -i -a
 brew update
 brew upgrade
 
+# Update Oh-my-zsh custom themes and plugins
+git -C ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k pull
+git -C ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions pull
+git -C ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting pull
+
 # Update npm packages
 npm update -g

e66f9687ea496d349b7afa6e26c6f7d24151ef1c
Pedro Pombeiro <noreply@pedro.pombei.ro>
2020-04-11 15:21:25 +0200

    oh-my-zsh: Increase history size

--- .shellrc/zshrc.d/configs/history.zsh
+++ .shellrc/zshrc.d/configs/history.zsh
@@ -1,4 +1,5 @@
-HISTSIZE=SAVEHIST=4000
+HISTSIZE=50000
+SAVEHIST=4000
 
 setopt INC_APPEND_HISTORY        # Write to the history file immediately, not when the shell exits.
 setopt SHARE_HISTORY             # Share history between all sessions.

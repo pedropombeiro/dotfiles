6352c4699d153f398b614c3866921a8116f40fa3
Pedro Pombeiro <noreply@pedro.pombei.ro>
2020-06-04 11:36:43 +0200

    gitlab: Ensure docker starts correctly

--- .shellrc/rc.d/gitlab/docker-machine.sh
+++ .shellrc/rc.d/gitlab/docker-machine.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 
 if command -v docker-machine &>/dev/null; then
-  # Configure default docker-machine VM
-  eval "$(docker-machine env docker)"
+  if [ "$(docker-machine status docker)" = "Running" ]; then
+    # Configure default docker-machine VM
+    eval "$(docker-machine env docker)"
+  else
+    # Ensure docker VM is started
+    docker-machine start docker
+    # Configure default docker-machine VM
+    eval "$(docker-machine env docker)"
+    # Install QEMU handlers
+    docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+  fi
 fi

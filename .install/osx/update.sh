2a990c6a3e39ab031ad599e674156f5126f2188d
Pedro Pombeiro <noreply@pedro.pombei.ro>
2020-06-04 10:48:06 +0200

    osx: Run brew cask upgrade

--- .install/osx/update.sh
+++ .install/osx/update.sh
@@ -6,11 +6,12 @@ sudo softwareupdate -i -a
 # Update Homebrew (Cask) & packages
 brew update
 brew upgrade
+brew cask upgrade
 
 # Update Oh-my-zsh custom themes and plugins
-git -C ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k pull
-git -C ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions pull
-git -C ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting pull
+git -C "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k" pull
+git -C "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions" pull
+git -C "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting" pull
 
 # Update npm packages
 npm update -g

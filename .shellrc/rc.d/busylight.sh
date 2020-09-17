37326c803eddafc98eecfcaee03898ac4f0eb451
Pedro Pombeiro <noreply@pedro.pombei.ro>
2020-09-17 07:42:00 +0200

    Add busylight aliases

---
+++ .shellrc/rc.d/busylight.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+alias busylight_red="curl 'http://localhost:8989?action=light&red=255'"
+alias busylight_green="curl 'http://localhost:8989?action=light&green=255'"
+alias busylight_off="curl 'http://localhost:8989?action=off'"

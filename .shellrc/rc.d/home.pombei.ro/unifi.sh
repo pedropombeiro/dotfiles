9f010678b4040ab2af19c4f6b11595535fcf9243
Pedro Pombeiro <noreply@pedro.pombei.ro>
2020-09-22 12:06:27 +0200

    unifi: Improve logging aliases

--- .shellrc/rc.d/home.pombei.ro/unifi.sh
+++ .shellrc/rc.d/home.pombei.ro/unifi.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-alias loggateway='ssh gateway "tail -f /var/log/messages"'
-alias logap='ssh ap "tcpdump -np"'
+alias loggateway='ssh gateway "tail -f /var/log/messages | sed -E \"s/( UniFiSecurityGateway3P kernel|-default| TOS=0x[0-9A-F]+| PREC=0x[0-9A-F]+| MAC=[0-9a-f:]*)//gm;t;d\"" | grep -E "\w[A-Z]+="'
+alias logap='ssh ap "tcpdump -np | sed \"s|, options \[.*\]||\""'

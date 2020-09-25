1c071f0cdaa9a9e6f77f0d17707d3813c26f30a7
Pedro Pombeiro <noreply@pedro.pombei.ro>
2020-09-25 08:59:26 +0200

    unifi: Improve logging aliases

--- .shellrc/rc.d/home.pombei.ro/unifi.sh
+++ .shellrc/rc.d/home.pombei.ro/unifi.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/env bash
 
-alias loggateway='ssh gateway "tail -f /var/log/messages | sed -E \"s/( UniFiSecurityGateway3P kernel|-default| TOS=0x[0-9A-F]+| PREC=0x[0-9A-F]+| MAC=[0-9a-f:]*)//gm;t;d\"" | grep -E "\w[A-Z]+="'
+function loggatewayjson() {
+  ssh gateway "tail -f /var/log/messages" | \
+    rg "kernel:" | \
+    sed "s/]IN/] IN/" | \
+    jq -R '. | rtrimstr(" ") | split(": ") | {date: .[0] | rtrimstr(" UniFiSecurityGateway3P kernel")} + (.[1] | capture("\\[(?<rule>.*)\\].*")) + ((.[1] | split(" ") | map(select(startswith("[") == false) | split("=") | {(.[0]): .[1]})) | (reduce .[] as $item ({}; . + $item)))'
+}
+
+function loggateway() {
+  loggatewayjson | jq -r '"\(.date) - \(.rule)\tIN=\(.IN) \t\(.PROTO)\tSRC=\(.SRC)@\(.SPT)\tDST=\(.DST)@\(.DPT)\tLEN=\(.LEN)\t"'
+}
+
+#alias loggateway='ssh gateway "tail -f /var/log/messages | sed -E \"s/( UniFiSecurityGateway3P kernel|-default| TOS=0x[0-9A-F]+| PREC=0x[0-9A-F]+| MAC=[0-9a-f:]*)//gm;t;d\"" | grep -E "\w[A-Z]+="'
 alias logap='ssh ap "tcpdump -np | sed \"s|, options \[.*\]||\""'

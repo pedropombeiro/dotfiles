5dc255a481b84add40e05c419b22390425608f49
Pedro Pombeiro <noreply@pedro.pombei.ro>
2020-10-04 14:30:21 +0200

    Remove commented out loggateway alias

--- .shellrc/rc.d/home.pombei.ro/unifi.sh
+++ .shellrc/rc.d/home.pombei.ro/unifi.sh
@@ -11,5 +11,4 @@ function loggateway() {
   loggatewayjson | jq -r '"\(.date) - \(.rule)\tIN=\(.IN) \t\(.PROTO)\tSRC=\(.SRC)@\(.SPT)\tDST=\(.DST)@\(.DPT)\tLEN=\(.LEN)\t"'
 }
 
-#alias loggateway='ssh gateway "tail -f /var/log/messages | sed -E \"s/( UniFiSecurityGateway3P kernel|-default| TOS=0x[0-9A-F]+| PREC=0x[0-9A-F]+| MAC=[0-9a-f:]*)//gm;t;d\"" | grep -E "\w[A-Z]+="'
 alias logap='ssh ap "tcpdump -np | sed \"s|, options \[.*\]||\""'

9dfe420ab95e304b1ae888a3a3434774e5b8abed
Pedro Pombeiro <noreply@pedro.pombei.ro>
2020-11-30 16:18:37 +0100

    go: Undefine GOROOT and rely on asdf

--- .shellrc/rc.d/go.sh
+++ .shellrc/rc.d/go.sh
@@ -2,7 +2,6 @@
 
 if command -v asdf >/dev/null && asdf current golang >/dev/null; then
   export GOPATH="${HOME}/go"
-  export GOROOT="$(asdf where golang)/go"
 
   export PATH="${PATH}:${GOPATH}/bin"
 

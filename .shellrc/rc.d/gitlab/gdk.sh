f457a879ddaa62f283788553224f0718dbf9d811
Pedro Pombeiro <noreply@pedro.pombei.ro>
2020-12-04 15:50:08 +0100

    gdk: Enable shell completion

--- .shellrc/rc.d/gitlab/gdk.sh
+++ .shellrc/rc.d/gitlab/gdk.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
 export RAILS_HOSTS="127.0.0.1,localhost,host.docker.internal,gdk.localhost"
+GDK_ROOT="${HOME}/src/gitlab-development-kit"
+[[ -d ${GDK_ROOT} ]] && export GDK_ROOT
--- .shellrc/zshrc.d/configs/gitlab/lab.zsh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env zsh
-
-if command -v lab >/dev/null; then
-  export FPATH=".shellrc/zshrc.d/configs/gitlab:${FPATH}"
-fi

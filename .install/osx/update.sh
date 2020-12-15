fe10d550df85b4c46e688e0cc0344126653a83d2
Pedro Pombeiro <noreply@pedro.pombei.ro>
2020-12-15 20:28:56 +0100

    osx: Enable TouchID after update

--- .install/osx/update.sh
+++ .install/osx/update.sh
@@ -15,3 +15,8 @@ brew upgrade
 
 # Update npm packages
 npm update -g
+
+# Enable TouchID on shell
+grep 'pam_tid.so' /etc/pam.d/sudo > /dev/null || \
+  echo -e "auth       sufficient     pam_tid.so\n$(cat /etc/pam.d/sudo)" | \
+  sudo tee /etc/pam.d/sudo

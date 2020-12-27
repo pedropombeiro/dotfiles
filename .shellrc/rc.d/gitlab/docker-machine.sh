5f21258ce313cd98384838d5769a9cfd01cea313
Pedro Pombeiro <noreply@pedro.pombei.ro>
2020-12-27 20:40:00 +0100

    zsh: Speed up GDK and docker-machine startup

--- .shellrc/rc.d/gitlab/docker-machine.sh
+++ .shellrc/rc.d/gitlab/docker-machine.sh
@@ -2,15 +2,14 @@
 
 dockerVMName='docker-parallels'
 if command -v docker-machine &>/dev/null; then
-  if [ "$(docker-machine status "${dockerVMName}")" = "Running" ]; then
-    # Configure default docker-machine VM
-    eval "$(docker-machine env "${dockerVMName}")"
-  else
+  if [ "$(docker-machine status "${dockerVMName}")" != "Running" ]; then
     # Ensure docker VM is started
     docker-machine start "${dockerVMName}"
-    # Configure default docker-machine VM
-    eval "$(docker-machine env "${dockerVMName}")"
     # Install QEMU handlers
     docker run --rm --privileged docker/binfmt:a7996909642ee92942dcd6cff44b9b95f08dad64
   fi
+
+  [[ -f ~/.docker-machine.env ]] || docker-machine env "${dockerVMName}" > ~/.docker-machine.env
+  # Configure default docker-machine VM
+  source ~/.docker-machine.env
 fi

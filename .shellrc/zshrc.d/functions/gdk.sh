6a356e73d919201bebb3fb7a514e96b6fd242e6f
Pedro Pombeiro <noreply@pedro.pombei.ro>
2021-06-23 09:41:33 +0200

    gdk: Improve fgdku function integration with rebase-all

--- .shellrc/zshrc.d/functions/gdk.sh
+++ .shellrc/zshrc.d/functions/gdk.sh
@@ -1,17 +1,21 @@
 #!/usr/bin/env bash
 
 if [[ -d "${GDK_ROOT}" ]]; then
-  function rebase-all() {
+  function _rebase-all() {
     CURRENT_BRANCH="$(git branch --show-current)"
 
+    DEFAULT_BRANCH='main'
+    git show-ref refs/remotes/origin/master >/dev/null && DEFAULT_BRANCH='master'
+    git for-each-ref --shell --format='git switch %(refname) && git rebase '"${DEFAULT_BRANCH}"' && echo -------- && '\\'' refs/heads/pedropombeiro | { sed 's;refs/heads/;;'; echo "git switch '${CURRENT_BRANCH}'"; } | bash
+  }
+
+  function rebase-all() {
     if ! git diff-index --quiet HEAD --; then
       echo 'Please stash the changes in the current branch before calling rebase-all!'
       return
     fi
 
-    DEFAULT_BRANCH='main'
-    git show-ref refs/remotes/origin/master >/dev/null && DEFAULT_BRANCH='master'
-    git for-each-ref --shell --format='git switch %(refname) && git rebase '"${DEFAULT_BRANCH}"' && echo -------- && '\\'' refs/heads/pedropombeiro | { sed 's;refs/heads/;;'; echo "git switch '${CURRENT_BRANCH}'"; } | bash
+    _rebase-all
   }
 
   function fgdku() {
@@ -28,11 +32,11 @@ if [[ -d "${GDK_ROOT}" ]]; then
     gdk update
     echo "Running simple test..."
     bin/rspec spec/lib/expand_variables_spec.rb
-    echo "Rebasing local branches..."
-    rebase-all
-    echo "Done."
-
     set +e
+
+    echo "Rebasing local branches..."
+    _rebase-all
+    echo "Done."
 
     cd - >/dev/null
   }

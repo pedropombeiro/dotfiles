2c016b4163e8ef41c33a47a249ce6e7f06ac4aa9
Pedro Pombeiro <noreply@pedro.pombei.ro>
2021-06-30 09:58:20 +0200

    gdk: Add some more useful aliases

--- .shellrc/zshrc.d/configs/post/gitlab/gdk.sh
+++ .shellrc/zshrc.d/configs/post/gitlab/gdk.sh
@@ -6,6 +6,9 @@ if [[ -d ${GDK_ROOT} ]]; then
 
   source "${GDK_ROOT}/support/completions/gdk.bash"
 
+  alias be="bundle exec "
+  alias compile_docs="bundle exec rake gitlab:graphql:compile_docs"
+  alias guard="SPRING=1 bundle exec guard"
   alias test-mr='bin/rspec $(git log master.. --oneline --name-only --pretty="format:" --diff-filter=CRAM | grep "spec/")'
 fi
 

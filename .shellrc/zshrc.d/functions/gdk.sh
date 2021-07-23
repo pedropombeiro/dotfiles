41e3e3fef5789155172ee8bf97b49fcdd8dec5dc
Pedro Pombeiro <noreply@pedro.pombei.ro>
2021-07-23 10:54:12 +0200

    git: Move branch-to function to a git alias

--- .shellrc/zshrc.d/functions/gdk.sh
+++ .shellrc/zshrc.d/functions/gdk.sh
@@ -45,13 +45,4 @@ if [[ -d "${GDK_ROOT}" ]]; then
 
     cd - >/dev/null
   }
-
-  function branch-to() {
-    if command -v "$1" >/dev/null; then
-      git log master.. --oneline --name-only --pretty="format:" --diff-filter=CRAM | xargs "$1"
-    else
-      echo "'$1' is not an executable. Please specify an editor such as 'code'." > /dev/stderr
-      return 1
-    fi
-  }
 fi

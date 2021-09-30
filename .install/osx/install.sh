df65d81eae1e613711e24bf1d4b258e9e735aa14
Pedro Pombeiro <noreply@pedro.pombei.ro>
2021-09-30 23:23:44 +0200

    osx: Upgrade asdf tools

--- .install/osx/install.sh
+++ .install/osx/install.sh
@@ -166,14 +166,13 @@ for p in golang golangci-lint hadolint nodejs yarn ruby shellcheck; do
 done
 asdf install golang 1.13.8 && asdf global golang 1.13.8
 asdf install golangci-lint 1.27.0 && asdf global golangci-lint 1.27.0
-asdf install hadolint v1.18.0 && asdf global hadolint v1.18.0
-asdf install shellcheck 0.7.1 && asdf global shellcheck 0.7.1
+echo "hadolint\nshellcheck" | xargs -I R bash -c 'asdf install R latest && asdf global R latest'
 
 bash -c '${ASDF_DATA_DIR:=$HOME/.asdf}/plugins/nodejs/bin/import-release-team-keyring'
 asdf install nodejs 14.5.0 && asdf global nodejs 14.5.0
 asdf install yarn 1.22.4 && asdf global yarn 1.22.4
 
-asdf install ruby 2.6.6 && asdf global ruby 2.6.6
+asdf install ruby 2.7.4 && asdf global ruby 2.7.4
 set -e
 
 source "${SCRIPT_DIR}/defaults.sh"

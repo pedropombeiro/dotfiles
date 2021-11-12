950c68fc928d8910c691bb5afad1b207fb086708
Pedro Pombeiro <noreply@pedro.pombei.ro>
2021-11-12 22:47:53 +0100

    zsh: Auto-install vim plugins

---
+++ .shellrc/zshrc.d/configs/post/vim-plugins.zsh
@@ -0,0 +1,13 @@
+#!/usr/bin/env zsh
+
+if [ "$(command -v vim)" ]; then
+  for plugin in sensible surround fugitive repeat commentary sleuth; do
+    if [[ ! -d ~/.vim/pack/tpope/start/${plugin} ]]; then
+      mkdir -p ~/.vim/pack/tpope/start
+      cd ~/.vim/pack/tpope/start
+      git clone https://tpope.io/vim/${plugin}.git
+      vim -u NONE -c "helptags ${plugin}/doc" -c q
+      cd -
+    fi
+  done
+fi

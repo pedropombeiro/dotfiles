fcaf9299b10fc17f059fe31badcd2a60255b184b
Pedro Pombeiro <noreply@pedro.pombei.ro>
2022-01-29 21:09:49 +0100

    ranger: Add rr alias

--- .shellrc/rc.d/aliases.sh
+++ .shellrc/rc.d/aliases.sh
@@ -3,4 +3,5 @@
 alias lg='[[ ${PWD} = ${HOME} ]] && lazygit -g $HOME/.local/share/yadm/repo.git -w ~ || lazygit'
 alias gdp='printf "\`\`\`patch\n%s\n\`\`\`" "$(git diff)" | pbcopy'
 alias sqlformat='pg_format --nocomment - | xargs -0 printf "\`\`\`sql\n%s\`\`\`" | pbcopy'
+alias rr=ranger
 alias vim=nvim

06ff35e7fbc816935de7b22c328e2ff8cece580f
Pedro Pombeiro <noreply@pedro.pombei.ro>
2022-02-03 23:36:25 +0100

    yadm: Repave ranger plugins if change is detected

--- .config/yadm/bootstrap.d/30-install-ranger-plugins.sh
+++ .config/yadm/bootstrap.d/30-install-ranger-plugins.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
+source "${HOME}/.config/yadm/scripts/colors.sh"
+
+plugins_dir="${HOME}/.config/ranger/plugins"
+
 function install-ranger-plugin() {
   local proj="$(basename $1)"
-  local target_dir="${HOME}/.config/ranger/plugins/${proj}"
+  local target_dir="${plugins_dir}/${proj}"
   if [[ ! -d "${target_dir}" ]]; then
     mkdir -p "$(dirname "${target_dir}")"
     git clone "$1" "${target_dir}"
@@ -11,12 +15,22 @@ function install-ranger-plugin() {
   return 0
 }
 
-install-ranger-plugin https://github.com/alexanderjeurissen/ranger_devicons
-install-ranger-plugin https://github.com/pedropombeiro/ranger-fzf
-install-ranger-plugin https://github.com/laggardkernel/ranger-fzf-marks
-install-ranger-plugin https://github.com/maximtrp/ranger-archives
-install-ranger-plugin https://github.com/alexanderjeurissen/ranger_devicons
+urls=(
+  'https://github.com/alexanderjeurissen/ranger_devicons'
+  'https://github.com/laggardkernel/ranger-fzf-marks'
+  'https://github.com/maximtrp/ranger-archives'
+  'https://github.com/pedropombeiro/ranger-fzf'
+)
+if [[ $(find "${plugins_dir}" -type d -mindepth 1 -maxdepth 1 | wc -l) -ne ${#urls[@]} ]]; then
+  printf "${YELLOW}%s${NC}\n" 'Change in number of Ranger plugins detected, repaving...'
+
+  rm -rf "${plugins_dir}"
+fi
+
+for url in ${urls[@]}; do
+  install-ranger-plugin "${url}"
+done
 
 if [[ ! -f "${HOME}/.config/ranger/plugins/autojump.py" ]]; then
-  curl -s --fail --location https://raw.githubusercontent.com/fdw/ranger-autojump/main/autojump.py --output "${HOME}/.config/ranger/plugins/autojump.py"
+  curl -s --fail --location https://raw.githubusercontent.com/fdw/ranger-autojump/main/autojump.py --output "${plugins_dir}/autojump.py"
 fi

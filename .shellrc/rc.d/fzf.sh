6ac58b5aad8ee815886f7522f93354ea124f9d1e
Pedro Pombeiro <noreply@pedro.pombei.ro>
2022-02-03 18:40:22 +0100

    fzf: Exclude common directories

--- .shellrc/rc.d/fzf.sh
+++ .shellrc/rc.d/fzf.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
-export FZF_DEFAULT_COMMAND='fd --type file --color=always'
+export FZF_DEFAULT_COMMAND='fd --type file --color=always --hidden --exclude .git --exclude=~/go/pkg'
 export FZF_DEFAULT_OPTS='--ansi --height 40% --inline-info --border'
+
+if [[ "$(uname -s)" = "Darwin" ]]; then
+  FZF_DEFAULT_COMMAND="${FZF_DEFAULT_COMMAND} --exclude=~/Library/Caches"
+fi
 
 function j() {
     if [[ "$#" -ne 0 ]]; then
-        cd $(autojump $@)
+        cd "$(autojump $@)"
         return
     fi
-    cd "$(autojump -s | sort -k1gr | awk '$1 ~ /[0-9]:/ && $2 ~ /^\// { for (i=2; i<=NF; i++) { print $(i) } }' |  fzf --height 40% --reverse --inline-info)"
+    cd "$(autojump -s | sort -k1gr | awk '$1 ~ /[0-9]:/ && $2 ~ /^\// { for (i=2; i<=NF; i++) { print $(i) } }' |  fzf --height 40% --reverse --inline-info)" || exit
 }

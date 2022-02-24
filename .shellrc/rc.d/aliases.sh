89f3975da78d6df3697b503e3e6b9c2ab279dab7
Pedro Pombeiro <noreply@pedro.pombei.ro>
2022-02-24 01:38:02 +0100

    yadm: Add lazygit configuration to repo

--- .shellrc/rc.d/aliases.sh
+++ .shellrc/rc.d/aliases.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-alias lg=lazygit
-alias ly='lazygit -w ~ -g ~/.local/share/yadm/repo.git'
+alias lg='lazygit --use-config-dir ~/.config/lazygit'
+alias ly='lazygit --use-config-dir ~/.config/lazygit --work-tree ~ --git-dir ~/.local/share/yadm/repo.git'
 alias gdp='printf "\`\`\`patch\n%s\n\`\`\`" "$(git diff)" | pbcopy'
 
 # Delete all remote tracking Git branches where the upstream branch has been deleted

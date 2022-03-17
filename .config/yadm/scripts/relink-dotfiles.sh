ccce0b67b579d8d09c340505ec8239b24e2680cb
Pedro Pombeiro <noreply@pedro.pombei.ro>
2022-03-17 22:00:14 +0100

    yadm: Add -r argument to xargs calls

--- .config/yadm/scripts/relink-dotfiles.sh
+++ .config/yadm/scripts/relink-dotfiles.sh
@@ -9,8 +9,8 @@ if [[ ${class} == 'Personal' || ${class} == 'Work' ]]; then
   src_path="${HOME}/Sync/pedro/.dotfiles/Home/MBP.${class}"
   if [[ -d ${src_path} ]]; then
     printf "${YELLOW}%s${NC}\n" "Linking .dotfiles in ${src_path} to ${HOME}..."
-    find ~ -type f -name '.*history' -maxdepth 1 -print0 | xargs -0 -n 1 -I {} cp {} "${src_path}/"
-    find "${src_path}" -type f -not -name '.sync-conflict*' -maxdepth 1 -print0 | xargs -0 -n 1 -I file bash -c "echo '> file' && ln -sf file ~/"
+    find ~ -type f -name '.*history' -maxdepth 1 -print0 | xargs -r -0 -n 1 -I {} cp {} "${src_path}/"
+    find "${src_path}" -type f -not -name '.sync-conflict*' -maxdepth 1 -print0 | xargs -r -0 -n 1 -I file bash -c "echo '> file' && ln -sf file ~/"
   else
     printf "${RED}%s${NC}\n" "${src_path} not found. Please configure Syncthing and perform a sync run first."
   fi
@@ -24,6 +24,6 @@ if [[ ${class} == 'Personal' || ${class} == 'Work' ]]; then
     fi
 
     printf "${YELLOW}%s${NC}\n" "Linking pgcli config to ${HOME}..."
-    find "${HOME}/Sync/pedro/.dotfiles/Home/MBP.${class}/.config/pgcli" -type f -print0 | xargs -0 -n 1 -I {} ln -sf {} "${HOME}/.config/pgcli/"
+    find "${HOME}/Sync/pedro/.dotfiles/Home/MBP.${class}/.config/pgcli" -type f -print0 | xargs -r -0 -n 1 -I {} ln -sf {} "${HOME}/.config/pgcli/"
   fi
 fi

4f1e00f509eeb0b28135f4bb1687bab7cadade1d
Pedro Pombeiro <noreply@pedro.pombei.ro>
2022-03-31 13:29:13 +0200

    gdk: Improve git_prune alias to include 'security' remote

--- .shellrc/rc.d/aliases.sh
+++ .shellrc/rc.d/aliases.sh
@@ -5,7 +5,7 @@ alias ly='lazygit --use-config-dir ~/.config/lazygit --work-tree ~ --git-dir ~/.
 alias gdp='printf "\`\`\`patch\n%s\n\`\`\`" "$(git diff)" | pbcopy'
 
 # Delete all remote tracking Git branches where the upstream branch has been deleted
-alias git_prune="git fetch --prune && git branch -vv | grep 'origin/.*: gone]' | awk '{print \$1}' | grep -v '^_.*$' | xargs git branch -D"
+alias git_prune="git fetch --prune && git branch -vv | grep -E '(origin|security)/.*: gone]' | awk '{print \$1}' | grep -v '^_.*$' | xargs git branch -D"
 
 alias sqlformat='pg_format --nocomment - | xargs -0 printf "\`\`\`sql\n%s\`\`\`" | pbcopy'
 alias rr=ranger

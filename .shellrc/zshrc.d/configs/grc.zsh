a16650dfe804d97876d07a0317757f5b9df4bf59
Pedro Pombeiro <noreply@pedro.pombei.ro>
2022-04-15 22:28:59 +0200

    zsh: Fix prefix for brew asdf and grc installs

--- .shellrc/zshrc.d/configs/grc.zsh
+++ .shellrc/zshrc.d/configs/grc.zsh
@@ -2,6 +2,11 @@
 
 if [[ -s /usr/local/etc/grc.zsh ]]; then
   source /usr/local/etc/grc.zsh
-elif [[ -s "/etc/grc.zsh" ]]; then
-  source /etc/grc.zsh
+else
+  BREW_PREFIX="$(brew --prefix 2>/dev/null)"
+  if [[ -s "${BREW_PREFIX}/etc/grc.zsh" ]]; then
+    source "${BREW_PREFIX}/etc/grc.zsh"
+  elif [[ -s /etc/grc.zsh ]]; then
+    source /etc/grc.zsh
+  fi
 fi

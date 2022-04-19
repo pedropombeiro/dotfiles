f78b1ef096950fd2a7de0a8f19c88bafbf3c4739
Pedro Pombeiro <noreply@pedro.pombei.ro>
2022-04-19 23:03:11 +0200

    yadm: Perform a shell performance test after each update

--- .config/yadm/scripts/update-common.zsh
+++ .config/yadm/scripts/update-common.zsh
@@ -14,5 +14,14 @@ omz update
 find "${ZSH_CUSTOM:-${HOME}/.oh-my-zsh/custom}/themes" -mindepth 1 -maxdepth 1 -type d -print0 | xargs -0 -P 8 -I {} git -C {} pull --prune --stat -v --ff-only
 find "${ZSH_CUSTOM:-${HOME}/.oh-my-zsh/custom}/plugins" -mindepth 1 -maxdepth 1 -type d -print0 | xargs -0 -P 8 -I {} git -C {} pull --prune --stat -v --ff-only
 
+printf "${YELLOW}%s${NC}\n" "Testing shell instantiation performance..."
+hf_file="$(mktemp)"
+hyperfine --warmup=1 --max-runs 5 'zsh -i -c exit' --export-json "${hf_file}"
+mean_time=$(jq '.results[].median' "${hf_file}")
+if [[ $mean_time -ge 0.6 ]]; then
+  printf "${RED}%s${NC}\n" "Zsh performance is too slow!"
+fi
+rm -f "${hf_file}"
+
 printf "${YELLOW}%s${NC}\n" "Updating vim plugins..."
 find "${HOME}/.vim/pack" -type d -name .git -print0 | xargs -0 -P 8 -I {} git -C {}/.. pull --prune --stat -v --ff-only && printf "${GREEN}%s${NC}\n" "Done"

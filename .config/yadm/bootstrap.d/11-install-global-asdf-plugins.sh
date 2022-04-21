dec66c0fc33aaed56d61eeab4d47d79a1dfb94cf
Pedro Pombeiro <noreply@pedro.pombei.ro>
2022-04-21 09:34:03 +0200

    yadm: Fix asdf update logic for QTS

--- .config/yadm/bootstrap.d/11-install-global-asdf-plugins.sh
+++ .config/yadm/bootstrap.d/11-install-global-asdf-plugins.sh
@@ -4,12 +4,12 @@ for p in hadolint shellcheck; do
   asdf plugin add "${p}"
 done
 
-asdf install hadolint v2.8.0 && asdf global hadolint v2.8.0
+asdf install hadolint v2.10.0 && asdf global hadolint v2.10.0
 asdf install shellcheck 0.8.0 && asdf global shellcheck 0.8.0
 
 if [[ $(uname -s) != 'Darwin' ]]; then
   asdf plugin add fzf
-  asdf install fzf 0.29.0 && asdf global fzf 0.29.0
+  asdf install fzf 0.30.0 && asdf global fzf 0.30.0
 
   if [[ ! -f ${HOME}/.fzf.zsh ]]; then
     printf "${YELLOW}%s${NC}\n" "Installing FZF scripts"

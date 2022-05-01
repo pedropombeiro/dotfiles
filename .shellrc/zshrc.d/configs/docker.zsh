15cd99fec88a6528d9e70194f30157984012c999
Pedro Pombeiro <noreply@pedro.pombei.ro>
2022-05-01 15:50:44 +0200

    zsh: Add Rancher Desktop path to environment

--- .shellrc/zshrc.d/configs/docker.zsh
+++ .shellrc/zshrc.d/configs/docker.zsh
@@ -1,5 +1,11 @@
 #!/usr/bin/env zsh
 
+if [[ -d ${HOME}/.rd/bin ]]; then
+  # Add Rancher Desktop path to environment
+  PATH="${HOME}/.rd/bin:$PATH"
+  export PATH
+fi
+
 if command -v docker > /dev/null; then
   plugins+=(docker docker-compose)
 elif command -v docker-machine > /dev/null; then

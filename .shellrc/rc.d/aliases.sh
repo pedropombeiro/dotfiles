00481bd8e042c7cd26dd827d12c901be8c6adb34
Pedro Pombeiro <noreply@pedro.pombei.ro>
2022-05-14 19:39:48 +0200

    zsh: Remove rr alias

--- .shellrc/rc.d/aliases.sh
+++ .shellrc/rc.d/aliases.sh
@@ -16,9 +16,8 @@ function y() {
 # Delete all remote tracking Git branches where the upstream branch has been deleted
 alias git_prune="git fetch --prune && git branch -vv | grep -E '(origin|security)/.*: gone]' | awk '{print \$1}' | grep -v '^_.*$' | xargs git branch -D"
 
-alias rr=ranger
 alias sqlformat='pg_format --nocomment - | xargs -0 printf "\`\`\`sql\n%s\`\`\`" | pbcopy'
 alias vim=nvim
 alias xh='xh -s monokai'
-command -v lsd >/dev/null && alias ls='lsd'
+alias ls='lsd'
 

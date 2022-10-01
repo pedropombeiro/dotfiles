f4f4e517b214db42ad8d015b8dc01950d7e127c1
Pedro Pombeiro <noreply@pedro.pombei.ro>
2022-10-01 15:20:12 +0200

    zsh: Remove unused docker plugin

--- .shellrc/zshrc.d/configs/docker.zsh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env zsh
-
-if [[ -d ${HOME}/.rd/bin ]]; then
-  # Add Rancher Desktop path to environment
-  PATH="${HOME}/.rd/bin:$PATH"
-  export PATH
-fi
-
-plugins+=(docker docker-compose)

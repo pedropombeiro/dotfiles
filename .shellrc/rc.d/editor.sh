c174b6d7b6b48ad3498825169d569b1011bb00fa
Pedro Pombeiro <noreply@pedro.pombei.ro>
2022-10-02 22:17:53 +0200

    zsh: Only load asdf plugin if needed

--- .shellrc/rc.d/editor.sh
+++ .shellrc/rc.d/editor.sh
@@ -2,9 +2,9 @@
 
 # Preferred editor for local and remote sessions
 if [ -n "$NVIM_LISTEN_ADDRESS" ]; then
-    export EDITOR="nvr -cc split --remote-wait +'set bufhidden=wipe'"
+  export EDITOR="nvr -cc split --remote-wait +'set bufhidden=wipe'"
 else
-    export EDITOR="nvim"
+  export EDITOR="nvim"
 fi
 
 export VISUAL=${EDITOR}
--- .shellrc/zshrc.d/configs/pre/asdf.zsh
+++ .shellrc/zshrc.d/configs/pre/asdf.zsh
@@ -1,2 +1,7 @@
-plugins+=(asdf)
-
+if [[ -f "${HOMEBREW_PREFIX}/opt/asdf/libexec/asdf.sh" ]]; then
+  source "${HOMEBREW_PREFIX}/opt/asdf/libexec/asdf.sh"
+else
+  if ! type asdf >/dev/null; then
+    plugins+=(asdf)
+  fi
+fi

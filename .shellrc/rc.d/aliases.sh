b12696f33f95bbe1fcea42e56bbc261c72d9a49c
Pedro Pombeiro <noreply@pedro.pombei.ro>
2022-10-02 12:44:11 +0200

    zsh: Move functions into autoload dir

--- .shellrc/rc.d/aliases.sh
+++ .shellrc/rc.d/aliases.sh
@@ -4,15 +4,6 @@ alias lg='lazygit --use-config-dir ~/.config/lazygit'
 alias ly='lazygit --use-config-dir ~/.config/lazygit --work-tree ~ --git-dir ~/.local/share/yadm/repo.git'
 alias gdp='printf "\`\`\`patch\n%s\n\`\`\`" "$(git diff)" | pbcopy'
 
-# Allow using y alias to run commands in the context of the YADM worktree
-function y() {
-  if [ $# -eq 0 ]; then
-    yadm enter "$SHELL"
-  else
-    yadm enter "$SHELL -i -c '$*'"
-  fi
-}
-
 # Delete all remote tracking Git branches where the upstream branch has been deleted
 alias git_prune="git fetch --prune && git branch -vv | grep -E '(origin|security)/.*: gone]' | awk '{print \$1}' | grep -v '^_.*$' | xargs git branch -D"
 
--- .shellrc/rc.d/fzf.sh
+++ .shellrc/rc.d/fzf.sh
@@ -8,11 +8,3 @@ export FZF_DEFAULT_COMMAND='fd --type file --color=always --hidden --exclude .gi
 if [[ "$(uname -s)" = "Darwin" ]]; then
   FZF_DEFAULT_COMMAND="${FZF_DEFAULT_COMMAND} --exclude=~/Library/Caches"
 fi
-
-function j() {
-  if [[ "$#" -ne 0 ]]; then
-    cd "$(autojump $@)"
-    return
-  fi
-  cd "$(autojump -s | sort -k1gr | awk '$1 ~ /[0-9]:/ && $2 ~ /^\// { for (i=2; i<=NF; i++) { print $(i) } }' |  fzf --height 40% --reverse --inline-info)" || exit
-}

741b2fcdb7826898d0a94f92fb3ed386250d7ba9
Pedro Pombeiro <noreply@pedro.pombei.ro>
2022-10-25 22:46:43 +0200

    zsh: Add vi mode key bindings for history-substring-search

--- .shellrc/zshrc.d/configs/post/history-substring-search.zsh
+++ .shellrc/zshrc.d/configs/post/history-substring-search.zsh
@@ -2,3 +2,6 @@
 
 # https://github.com/zsh-users/zsh-history-substring-search/blob/master/README.md#install
 plugins+=(history-substring-search)
+
+bindkey -M vicmd 'k' history-substring-search-up
+bindkey -M vicmd 'j' history-substring-search-down

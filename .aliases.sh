103ede17b1d2c5afe1c5b68d652569135381d4f2
Pedro Pombeiro <noreply@pedro.pombei.ro>
2022-11-10 23:41:43 +0100

    vim: Use neovim for vimdiff

--- .aliases.sh
+++ .aliases.sh
@@ -1,13 +1,14 @@
+#!/usr/bin/env bash
 # Set personal aliases, overriding those provided by oh-my-zsh libs,
 # plugins, and themes. Aliases can be placed here, though oh-my-zsh
 # users are encouraged to define aliases within the ZSH_CUSTOM folder.
 # For a full list of active aliases, run `alias`.
 #
 # Example aliases
-alias zshconfig="$EDITOR ~/.zshrc"
-alias ohmyzsh="$EDITOR ~/.oh-my-zsh"
+alias ohmyzsh="\$EDITOR ~/.oh-my-zsh"
 alias mkcd='foo(){ mkdir -p "$1"; cd "$1" }; foo'
 alias myip="curl http://ipecho.net/plain; echo"
+alias vimdiff="vim -d"
 
 # Include custom aliases
 if [[ -f ~/.aliases.local ]]; then

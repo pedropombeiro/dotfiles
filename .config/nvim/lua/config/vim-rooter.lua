9d9e28e412111355f80216158cc690299f629c44
Pedro Pombeiro <noreply@pedro.pombei.ro>
2022-11-14 00:29:59 +0100

    Revert "vim: Install vim-rooter"
    This reverts commit 585dd16592ba270d70f4f8d4debe50010873b1f9.

--- .config/nvim/lua/config/vim-rooter.lua
+++
@@ -1,9 +0,0 @@
--- vim-rooter (https://github.com/airblade/vim-rooter)
---  Changes Vim working directory to project root.
-
-vim.g.rooter_patterns = {
-  "*.sln",
-  ".git",
-  "Gemfile",
-  "Makefile",
-}

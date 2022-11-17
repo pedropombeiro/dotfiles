95651ea92de211f9d560d09c407e57f10ee8bfe7
Pedro Pombeiro <noreply@pedro.pombei.ro>
2022-11-17 12:11:33 +0100

    vim: Fix vim theme in firenvim where environment variable is not available

--- .config/nvim/lua/config/themer.lua
+++ .config/nvim/lua/config/themer.lua
@@ -1,8 +1,13 @@
 -- themer.lua (https://github.com/themercorp/themer.lua)
 --  A simple, minimal highlighter plugin for neovim
 
+local theme = vim.env.NVIM_THEME -- defined in ~/.shellrc/rc.d/theme.sh
+if theme == nil then
+  theme = "gruvbox"
+end
+
 require("themer").setup({
-  colorscheme = vim.env.NVIM_THEME, -- defined in ~/.shellrc/rc.d/theme.sh
+  colorscheme = theme,
   langs = {
     html = true,
     md   = true,

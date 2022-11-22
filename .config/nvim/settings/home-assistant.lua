28b54b5994d4e7fdc28bba9d2ff711be89464386
Pedro Pombeiro <noreply@pedro.pombei.ro>
2022-11-22 23:18:17 +0100

    vim: Convert home-assistant.vim to lua

---
+++ .config/nvim/settings/home-assistant.lua
@@ -0,0 +1,7 @@
+local ha_augroup = vim.api.nvim_create_augroup('homeassistant', { clear = true })
+
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = { "*/config/home-assistant/*.yaml", "*/config/home-assistant/**/*.yaml" },
+  group = ha_augroup,
+  command = "setlocal filetype=home-assistant syntax=yaml"
+})
--- .config/nvim/settings/home-assistant.vim
+++
@@ -1,4 +0,0 @@
-augroup homeassistant
-  autocmd BufRead,BufNewFile */config/home-assistant/*.yaml setlocal filetype=home-assistant syntax=yaml
-  autocmd BufRead,BufNewFile */config/home-assistant/**/*.yaml setlocal filetype=home-assistant syntax=yaml
-augroup END

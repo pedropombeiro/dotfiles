57165ee6423bec76a8fceae86c8eb90a3f0ed664
Pedro Pombeiro <noreply@pedro.pombei.ro>
2022-11-22 09:06:03 +0100

    vim: Remove code to load defaults.vim, as it doesn't exist in Neovim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -13,11 +13,6 @@ vim.api.nvim_command("autocmd!")
 vim.g.mapleader = " "
 vim.opt.timeoutlen = 500
 
--- Get the defaults that most users want.
-if vim.fn.filereadable(vim.fn.expand("$VIMRUNTIME/defaults.vim")) ~= 0 then
-  vim.cmd("source \\$VIMRUNTIME/defaults.vim")
-end
-
 -- Inspiration: https://github.com/skwp/dotfiles/blob/master/vim/settings.vim
 local vimsettings = "~/.config/nvim/settings"
 local settingsfiles = vim.fn.split(vim.fn.globpath(vimsettings, "*.{lua,vim}"), "\n")

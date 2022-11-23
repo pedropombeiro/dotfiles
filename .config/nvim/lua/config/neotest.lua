ed614046fd4bb845893d64a7e45aaf8ad0ff6024
Pedro Pombeiro <noreply@pedro.pombei.ro>
2022-11-23 12:52:18 +0100

    vim: Apply Gruvbox Dark theme to Neotest

--- .config/nvim/lua/config/neotest.lua
+++ .config/nvim/lua/config/neotest.lua
@@ -31,9 +31,52 @@ neotest.setup({
       end
     }),
   },
+  icons = {
+    expanded = "",
+    child_prefix = "",
+    child_indent = "",
+    final_child_prefix = "",
+    non_collapsible = "",
+    collapsed = "",
+
+    passed = "",
+    running = "",
+    failed = "",
+    unknown = ""
+  },
 })
 
+-- Theming (from https://github.com/nvim-neotest/neotest/blob/master/lua/neotest/config/init.lua)
+local function define_highlights()
+  vim.cmd([[
+  hi default NeotestPassed ctermfg=Green guifg=#b8bb26
+  hi default NeotestFailed ctermfg=Red guifg=#fb4934
+  hi default NeotestRunning ctermfg=Yellow guifg=#fabd2f
+  hi default NeotestSkipped ctermfg=Cyan guifg=#83a598
+  hi default link NeotestTest Normal
+  hi default NeotestNamespace ctermfg=Magenta guifg=#d3869b
+  hi default NeotestFocused gui=bold,underline cterm=bold,underline
+  hi default NeotestFile ctermfg=Cyan guifg=#83a598
+  hi default NeotestDir ctermfg=Cyan guifg=#83a598
+  hi default NeotestIndent ctermfg=Grey guifg=#a89984
+  hi default NeotestExpandMarker ctermfg=Grey guifg=#bdae93
+  hi default NeotestAdapterName ctermfg=Red guifg=#cc241d
+  hi default NeotestWinSelect ctermfg=Cyan guifg=#83a598 gui=bold
+  hi default NeotestMarked ctermfg=Brown guifg=#fe8019 gui=bold
+  hi default NeotestTarget ctermfg=Red guifg=#cc241d
+  hi default link NeotestUnknown Normal
+  ]])
+end
 
+--- Override Neotest default theme
+local theme = vim.env.NVIM_THEME -- defined in ~/.shellrc/rc.d/theme.sh
+if theme == "gruvbox" then
+  local augroup = vim.api.nvim_create_augroup("NeotestColorSchemeRefresh", { clear = true })
+  vim.api.nvim_create_autocmd("ColorScheme", { callback = define_highlights, group = augroup })
+  define_highlights()
+end
+
+-- Keymaps
 local opts = { noremap = true, silent = true }
 vim.keymap.set("n", "<leader>rt", neotest.run.run, opts)
 vim.keymap.set("n", "<leader>rT", function() neotest.run.run(vim.fn.expand("%")) end, opts)

6a2507f928bcb5ccae9cc9dda9aaca3f0647bffe
Pedro Pombeiro <noreply@pedro.pombei.ro>
2022-11-25 09:41:01 +0100

    vim: Make LazyGit behave correctly when Vim is started from Ranger

--- .config/nvim/lua/core/yadm-helper.lua
+++ .config/nvim/lua/core/yadm-helper.lua
@@ -1,6 +1,12 @@
--- If vim is started from the home directory, assume that GIT_DIR points to yadm repo
+-- If vim is started from the home or config directories, assume that GIT_DIR points to yadm repo
 --  (makes it possible to use LazyGit directly)
 
-if vim.env.GIT_DIR == nil and vim.fn.getcwd() == vim.fn.expand("~") then
-  vim.env.GIT_DIR = vim.fn.expand("~/.local/share/yadm/repo.git")
+if vim.env.GIT_DIR == nil then
+  local cwd = vim.fn.getcwd()
+  local homedir = vim.fn.expand("~")
+  local configdir = vim.fn.expand("~/.config")
+
+  if cwd == homedir or string.sub(cwd, 1, #configdir) == configdir then
+    vim.env.GIT_DIR = vim.fn.expand("~/.local/share/yadm/repo.git")
+  end
 end
--- .config/nvim/lua/plugins/lazygit.lua
+++ .config/nvim/lua/plugins/lazygit.lua
@@ -3,4 +3,12 @@
 
 -- setup mapping to call :LazyGit
 local opts = { noremap = true, silent = true }
-vim.keymap.set("n", "<leader>gg", "<Cmd>LazyGit<CR>", opts)
+local function openLazyGit()
+  if vim.env.GIT_DIR == vim.fn.expand("~/.local/share/yadm/repo.git") then
+    -- Ensure that we're located at the repository root, so that LazyGit correctly displays diffs
+    vim.cmd("cd ~")
+  end
+  vim.cmd("LazyGit")
+end
+
+vim.keymap.set("n", "<leader>gg", openLazyGit, opts)

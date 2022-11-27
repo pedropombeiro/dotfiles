3454a0666153bd8be889d44e24e556b7c0a9b85c
Pedro Pombeiro <noreply@pedro.pombei.ro>
2022-11-27 15:47:24 +0100

    vim: Register vim-unimpaired key mappings with which-key

--- .config/nvim/lua/core/keymap-plugins.lua
+++ .config/nvim/lua/core/keymap-plugins.lua
@@ -65,3 +65,47 @@ wk.register({
 
 -- vim-unimpaired
 m.nname("=", "Unimpaired")
+m.nname("[o", "Unimpaired - Enable")
+m.nname("]o", "Unimpaired - Disable")
+local unimpaired_option_mappings = {
+  b = { "background" },
+  c = { "cursorline" },
+  d = { "diff" },
+  h = { "hlsearch" },
+  i = { "ignorecase" },
+  l = { "list" },
+  n = { "number" },
+  r = { "relativenumber" },
+  s = { "spell" },
+  t = { "colorcolumn" },
+  u = { "cursorcolumn" },
+  v = { "virtualedit" },
+  w = { "wrap" },
+  x = { "cursorline + cursorcolumn" },
+}
+wk.register(unimpaired_option_mappings, { prefix = "[o" })
+wk.register(unimpaired_option_mappings, { prefix = "]o" })
+wk.register({
+  ["[f"] = { "Go to prev file in folder" },
+  ["]f"] = { "Go to next file in folder" },
+  ["[n"] = { "Go to prev conflict/diff/hunk" },
+  ["]n"] = { "Go to next conflict/diff/hunk" },
+  ["[e"] = { "Exchange line with lines above" },
+  ["]e"] = { "Exchange line with lines below" },
+  ["[<Space>"] = { "Add empty lines above" },
+  ["]<Space>"] = { "Add empty lines below" },
+})
+wk.register({
+  ["[u"] = { "URL encode" },
+  ["]u"] = { "URL decode" },
+  ["[x"] = { "XML encode" },
+  ["]x"] = { "XML decode" },
+  ["[C"] = { "C String encode" },
+  ["]C"] = { "C String decode" },
+  ["[y"] = { "C String encode" },
+  ["]y"] = { "C String decode" },
+}, { mode = { "n", "v" } })
+
+m.nname("=", "Unimpaired - Paste (reindending)")
+m.nname("<", "Unimpaired - Paste before linewise")
+m.nname(">", "Unimpaired - Paste after linewise")

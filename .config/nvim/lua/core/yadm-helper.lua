b49b9eff80349876d90bde2e5d55864adfde65fd
Pedro Pombeiro <noreply@pedro.pombei.ro>
2022-12-11 23:20:56 +0100

    vim: Simplify yadm helper logic

--- .config/nvim/lua/core/yadm-helper.lua
+++ .config/nvim/lua/core/yadm-helper.lua
@@ -2,11 +2,12 @@
 --  (makes it possible to use LazyGit directly)
 
 if vim.env.GIT_DIR == nil then
-  local cwd = vim.fn.getcwd()
-  local homedir = vim.fn.expand("~")
+  local cwd       = vim.fn.getcwd()
+  local homedir   = vim.fn.expand("~")
   local configdir = vim.fn.expand("~/.config")
 
   if cwd == homedir or string.sub(cwd, 1, #configdir) == configdir then
     vim.env.GIT_DIR = vim.fn.expand("~/.local/share/yadm/repo.git")
+    vim.cmd("cd ~")
   end
 end

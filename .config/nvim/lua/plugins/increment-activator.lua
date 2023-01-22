31407989a0a508886ca6852c640816aa55201adc
Pedro Pombeiro <noreply@pedro.pombei.ro>
2023-01-22 22:03:46 +0100

    vim: Improve increment-activator config

--- .config/nvim/lua/plugins/increment-activator.lua
+++ .config/nvim/lua/plugins/increment-activator.lua
@@ -5,13 +5,15 @@ return {
   "nishigori/increment-activator",
   init = function()
     vim.g.increment_activator_filetype_candidates = {
+      ["_"] = {
+        { "enable", "disable" },
+      },
       ["home-assistant"] = {
         { "turn_off", "turn_on" },
         { "unavailable", "available" },
         { "not_home", "home" },
       },
       ruby = {
-        { "enable", "disable" },
         { "be_allowed", "be_disallowed" },
         { "to", "not_to" },
       },

79acb6b3ad2d0f1674db4cc7ca44ee63ccb990d5
Pedro Pombeiro <noreply@pedro.pombei.ro>
2023-02-14 13:04:59 +0100

    vim: Fix firenvim#install not being available

--- .config/nvim/lua/plugins/firenvim.lua
+++ .config/nvim/lua/plugins/firenvim.lua
@@ -3,7 +3,6 @@
 
 return {
   "glacambre/firenvim",
-  cond = function() return vim.g.started_by_firenvim end,
   build = function() vim.fn["firenvim#install"](0) end,
   init = function()
     if vim.g.started_by_firenvim then

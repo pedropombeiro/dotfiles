181d14e70f6d70af4863d0492956aaa3285f826e
Pedro Pombeiro <noreply@pedro.pombei.ro>
2023-02-15 19:12:22 +0100

    Update UniFi OS to 2.4

--- .shellrc/rc.d/home.pombei.ro/unifi.sh
+++ .shellrc/rc.d/home.pombei.ro/unifi.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 function loggatewayjson() {
-  ssh gateway "tail -f -n 100 /var/log/messages" | \
+  ssh gateway "tail -f -n 100 /var/log/ulog/syslogemu.log" | \
     rg "kernel:" | \
     sed "s/]IN/] IN/" | \
     jq --unbuffered -R '. | rtrimstr(" ") | split(": ") | {date: (.[0] | split(" ") | .[0:4] | join(" "))} + (.[1] | capture("\\[.+\\] \\[(?<rule>.*)\\].*")) + ((.[1] | capture("\\[.+\\] (?<rest>.*)") | .rest | split(" ") | map(select(startswith("[") == false) | split("=") | {(.[0]): .[1]})) | (reduce .[] as $item ({}; . + $item)))'
@@ -9,14 +9,10 @@ function loggatewayjson() {
 
 function loggateway() {
   if command -v lnav >/dev/null; then
-    ssh -fn gateway 'tail -fqn 100000 /var/log/messages' 2>/dev/null | lnav
+    ssh -fn gateway 'tail -fqn 100000 /var/log/ulog/syslogemu.log' 2>/dev/null | lnav
   else
     loggatewayjson | jq --unbuffered -r '"\(.date) - \(.rule)\tIN=\(.IN)  \t\(.PROTO)\tSRC=\(.SRC)@\(.SPT)\tDST=\(.DST)@\(.DPT)\tLEN=\(.LEN)\t"'
   fi
-}
-
-function logfirewall() {
-  ssh -fn gateway 'tail -fqn 100000 /var/log/iptables' 2>/dev/null | lnav
 }
 
 alias logap='ssh ap "tcpdump -np | sed \"s|, options \[.*\]||\""'

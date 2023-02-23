b9e2b1f581a803a2c10a91f873d4a005d7edca8b
Pedro Pombeiro <noreply@pedro.pombei.ro>
2023-02-23 23:19:47 +0100

    vim: Add .markdownlintrc default config

--- .config/nvim/ftplugin/markdown.lua
+++ .config/nvim/ftplugin/markdown.lua
@@ -1,6 +1,6 @@
 -- Enable spellchecking
-vim.opt_local.spell = true
+vim.opt_local.spell       = true
 
 -- Automatically wrap at 100 characters
-vim.opt_local.textwidth   = 100
-vim.opt_local.colorcolumn = "100"
+vim.opt_local.textwidth   = 120
+vim.opt_local.colorcolumn = "120"

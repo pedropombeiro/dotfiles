b93b1fe852d942fd998841a3f74ee06617aaedf0
Pedro Pombeiro <noreply@pedro.pombei.ro>
2023-02-26 22:53:00 +0100

    vim: Simplify git-messenger config

--- .config/nvim/lua/plugins/git-messenger.lua
+++ .config/nvim/lua/plugins/git-messenger.lua
@@ -1,15 +1,12 @@
 -- git-messenger.vim (https://github.com/rhysd/git-messenger.vim)
 --  Vim and Neovim plugin to reveal the commit messages under the cursor
 
+vim.g.git_messenger_no_default_mappings = true
+
 return {
   "rhysd/git-messenger.vim", -- Vim and Neovim plugin to reveal the commit messages under the cursor
   cmd = "GitMessenger",
-  keys = "<leader>gm",
-  init = function()
-    local wk = require("which-key")
-
-    wk.register({
-      ["gm"] = { "Open commit message" }
-    }, { prefix = "<leader>" })
-  end
+  keys = {
+    { "<leader>gm", "<Plug>(git-messenger)", desc = "Open commit message" },
+  }
 }

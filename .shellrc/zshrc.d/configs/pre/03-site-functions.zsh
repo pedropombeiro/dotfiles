2d7d45ba7a5209a5b60f6d66ab18bd49bb0290d7
Pedro Pombeiro <noreply@pedro.pombei.ro>
2023-03-05 22:59:09 +0100

    zsh: Make oh-my-zsh.sh recreate any compdump older than a day

--- .shellrc/zshrc.d/configs/pre/03-site-functions.zsh
+++ .shellrc/zshrc.d/configs/pre/03-site-functions.zsh
@@ -2,3 +2,6 @@
 
 mkdir -p $HOME/.config/zsh/site-functions
 fpath+=($HOME/.config/zsh/site-functions)
+
+# make oh-my-zsh.sh recreate any compdump older than a day
+find $HOME -maxdepth 1 -iname '.zcompdump*' -mtime 1 -delete

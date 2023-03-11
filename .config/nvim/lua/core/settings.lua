b0cdaee2120d03ac22d2362a0c6b815b25cbed82
Pedro Pombeiro <noreply@pedro.pombei.ro>
2023-03-11 19:25:17 +0100

    vim: Do not show ending whitespaces on help files

--- .config/nvim/lua/core/settings.lua
+++ .config/nvim/lua/core/settings.lua
@@ -1,29 +1,29 @@
 --- General config ------------------------------------------------------------------
 
 vim.opt.shortmess:append("I")
-vim.opt.smartindent = true
-vim.opt.swapfile    = false -- disable the swapfile
-vim.opt.history     = 2000
-vim.opt.updatetime  = 1000
-vim.opt.wildmenu    = true -- set zsh-alike autocomplete behavior
-vim.opt.wildmode    = "full"
-vim.opt.expandtab   = true
-vim.opt.tabpagemax  = 40 -- Max number of tab pages that can be opened from the command line
-vim.opt.errorbells  = false
-vim.opt.confirm     = true -- Display a confirmation dialog when closing a dirty buffer (Mastering Vim Quickly)
+vim.opt.smartindent          = true
+vim.opt.swapfile             = false -- disable the swapfile
+vim.opt.history              = 2000
+vim.opt.updatetime           = 1000
+vim.opt.wildmenu             = true -- set zsh-alike autocomplete behavior
+vim.opt.wildmode             = "full"
+vim.opt.expandtab            = true
+vim.opt.tabpagemax           = 40 -- Max number of tab pages that can be opened from the command line
+vim.opt.errorbells           = false
+vim.opt.confirm              = true -- Display a confirmation dialog when closing a dirty buffer (Mastering Vim Quickly)
 
-vim.g.loaded_perl_provider = 0 -- disable Perl support
+vim.g.loaded_perl_provider   = 0 -- disable Perl support
 
 -- This makes vim act like all other editors, buffers can
 -- exist in the background without being in a window.
 -- http://items.sjbach.com/319/configuring-vim-right
-vim.opt.hidden = true
+vim.opt.hidden               = true
 
 -- Make macros render faster (lazy draw)
-vim.opt.lazyredraw = true
+vim.opt.lazyredraw           = true
 
 ---------------- Persistent Undo ------------------
-vim.opt.undofile = true
+vim.opt.undofile             = true
 local vimrc_undofile_augroup = vim.api.nvim_create_augroup("vimrc_undofile", { clear = true })
 vim.api.nvim_create_autocmd("BufWritePre", {
   pattern = "/tmp/*",
@@ -32,8 +32,8 @@ vim.api.nvim_create_autocmd("BufWritePre", {
 })
 
 ---------------- Spelling ------------------
-vim.opt.spelllang = "en_us"
-vim.opt.spell = false
+vim.opt.spelllang        = "en_us"
+vim.opt.spell            = false
 
 -- netrw customization (https://shapeshed.com/vim-netrw/)
 vim.g.netrw_liststyle    = 1 -- wide view
@@ -42,8 +42,8 @@ vim.g.netrw_altv         = 1
 vim.g.netrw_winsize      = 25
 
 ---------------- Security --------------------------
-vim.opt.modelines = 0
-vim.opt.modeline  = false
+vim.opt.modelines        = 0
+vim.opt.modeline         = false
 
 -- Q: Mouse reporting in vim doesn't work for some rows/columns in a big terminal window.
 if vim.fn.has("mouse_sgr") ~= 0 then
@@ -52,14 +52,14 @@ end
 
 ---------------- Layout --------------------------
 
-vim.opt.title      = false -- change terms title
-vim.opt.number     = true -- show line numbers
-vim.opt.ruler      = true -- show ruler in status line
-vim.opt.showmode   = true -- always show mode
-vim.opt.laststatus = 2 -- always show status line
-vim.opt.showcmd    = true -- show the command being typed
-vim.opt.scrolloff  = 4 -- keep 4 lines off the edges
-vim.opt.pumheight  = 10 -- popup menu height
+vim.opt.title          = false -- change terms title
+vim.opt.number         = true -- show line numbers
+vim.opt.ruler          = true -- show ruler in status line
+vim.opt.showmode       = true -- always show mode
+vim.opt.laststatus     = 2 -- always show status line
+vim.opt.showcmd        = true -- show the command being typed
+vim.opt.scrolloff      = 4 -- keep 4 lines off the edges
+vim.opt.pumheight      = 10 -- popup menu height
 
 vim.opt.foldlevelstart = 99
 
@@ -81,23 +81,23 @@ vim.api.nvim_create_autocmd("InsertLeave", {
 
 ---------------- Search --------------------------
 
-vim.opt.incsearch  = true -- Find the next match as we type the search
-vim.opt.hlsearch   = true -- Highlight searches by default
-vim.opt.ignorecase = true -- Ignore case when searching...
-vim.opt.smartcase  = true -- ...unless we type a capital
-vim.opt.grepprg    = "rg --vimgrep --smart-case --follow"
+vim.opt.incsearch    = true -- Find the next match as we type the search
+vim.opt.hlsearch     = true -- Highlight searches by default
+vim.opt.ignorecase   = true -- Ignore case when searching...
+vim.opt.smartcase    = true -- ...unless we type a capital
+vim.opt.grepprg      = "rg --vimgrep --smart-case --follow"
 
 ---------------- Format --------------------------
 
 vim.opt.encoding     = "utf-8"
 vim.opt.termencoding = "utf-8"
 vim.opt.fileformats  = "unix,dos,mac" -- supported formats
-vim.opt.bomb         = false -- don't use a BOM
+vim.opt.bomb         = false          -- don't use a BOM
 vim.opt.shiftwidth   = 2
 vim.opt.softtabstop  = 2
 vim.opt.tabstop      = 2
 
-vim.opt.listchars = {
+vim.opt.listchars    = {
   precedes = "⟨",
   extends = "⟩",
   eol = "↵",
@@ -105,7 +105,7 @@ vim.opt.listchars = {
   tab = "│·",
   nbsp = "␣"
 }
-vim.opt.list = true
+vim.opt.list         = true
 
 -- highlight trailing whitespace (Mastering Vim Quickly)
 vim.cmd([[
@@ -117,7 +117,7 @@ vim.cmd([[
   augroup TrailingSpace
     au!
     autocmd BufNewFile,BufRead * call HighlightExtraWhitespace()
-    autocmd FileType alpha,neo-tree highlight clear ExtraWhitespace
+    autocmd FileType alpha,help,neo-tree highlight clear ExtraWhitespace
   augroup END
 ]])
 

1d9ae5e2e11c688059de4d0a8ce3dd83ed906a21
Pedro Pombeiro <noreply@pedro.pombei.ro>
2023-03-12 18:38:41 +0100

    zsh: Hide fzf preview window by default

--- .shellrc/rc.d/fzf.sh
+++ .shellrc/rc.d/fzf.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 ## FZF_DEFAULT_COLOR defined in ~/.shellrc/rc.d/_theme.sh
-export FZF_DEFAULT_OPTS="--height 40% --inline-info --border --ansi --bind ctrl-_:toggle-preview ${FZF_DEFAULT_COLOR} --preview 'bat --color=always --style=header,grid --line-range :300 {}'"
+export FZF_DEFAULT_OPTS="--height 40% --inline-info --border --ansi --bind ctrl-_:toggle-preview ${FZF_DEFAULT_COLOR} --preview 'bat --color=always --style=header,grid --line-range :300 {}' --preview-window hidden"
 
 export FZF_DEFAULT_COMMAND='fd --type file --color=always --hidden --exclude .git --exclude=~/go/pkg'
 if [[ "$(uname -s)" = "Darwin" ]]; then

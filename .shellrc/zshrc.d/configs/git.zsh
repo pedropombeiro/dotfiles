3efb017be931c0915f47728d793b480cfcf90990
Pedro Pombeiro <noreply@pedro.pombei.ro>
2023-03-13 22:41:11 +0100

    zsh: Add git jump to PATH

--- .shellrc/zshrc.d/configs/git.zsh
+++ .shellrc/zshrc.d/configs/git.zsh
@@ -3,3 +3,10 @@
 plugins+=(git git-extras)
 
 export GIT_COMPLETION_CHECKOUT_NO_GUESS=1 # only autocomplete with local branches
+
+if [[ -n ${HOMEBREW_PREFIX} ]]; then
+  PATH="${PATH}:${HOMEBREW_PREFIX}/opt/git/share/git-core/contrib/git-jump"
+  export PATH
+fi
+
+export GIT_EDITOR="nvim -c 'Trouble quickfix'" # Open 'git jump' results directly in quickfix window

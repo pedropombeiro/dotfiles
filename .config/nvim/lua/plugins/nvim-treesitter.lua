819679fbd08464154dbb97d18ac339ca0e5d4be4
Pedro Pombeiro <noreply@pedro.pombei.ro>
2023-03-14 17:49:33 +0100

    vim: Fix nvim-treesitter config

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -11,12 +11,12 @@ return {
     "nvim-treesitter/nvim-treesitter-context", -- Show code context
     event = "BufReadPre",
     config = function()
-      local config = require("config")
+      ---@diagnostic disable-next-line: undefined-field
+      local colors = require("config").theme.colors
 
       require("treesitter-context").setup()
 
-      ---@diagnostic disable-next-line: undefined-field
-      vim.api.nvim_set_hl(0, "TreesitterContextBottom", { underline = true, sp = config.theme.bg0_s })
+      vim.api.nvim_set_hl(0, "TreesitterContextBottom", { underline = true, sp = colors.bg0_s })
     end,
   },
 
@@ -106,48 +106,45 @@ return {
           lookahead = true,
           keymaps = {
             -- You can use the capture groups defined in textobjects.scm
-                ["af"] = "@function.outer",
-                ["if"] = "@function.inner",
-                ["ac"] = "@class.outer",
-                ["ic"] = "@class.inner"
+            ["af"] = "@function.outer",
+            ["if"] = "@function.inner",
+            ["ac"] = "@class.outer",
+            ["ic"] = "@class.inner"
           }
         },
         move = {
           enable = true,
           set_jumps = true,
           goto_next_start = {
-                ["]m"] = "@function.outer",
-                ["]]"] = "@class.outer"
+            ["]m"] = "@function.outer",
+            ["]]"] = "@class.outer"
           },
           goto_next_end = {
-                ["]M"] = "@function.outer",
-                ["]["] = "@class.outer"
+            ["]M"] = "@function.outer",
+            ["]["] = "@class.outer"
           },
           goto_previous_start = {
-                ["[m"] = "@function.outer",
-                ["[["] = "@class.outer"
+            ["[m"] = "@function.outer",
+            ["[["] = "@class.outer"
           },
           goto_previous_end = {
-                ["[M"] = "@function.outer",
-                ["[]"] = "@class.outer"
+            ["[M"] = "@function.outer",
+            ["[]"] = "@class.outer"
           }
         },
         swap = {
           enable = true,
           swap_next = {
-                ["<leader>."] = "@parameter.inner",
+            ["<leader>."] = "@parameter.inner",
           },
           swap_previous = {
-                ["<leader>,"] = "@parameter.inner",
+            ["<leader>,"] = "@parameter.inner",
           },
         },
       },
     },
     ---@param opts TSConfig
     config = function(plugin, opts)
-      if plugin.ensure_installed then
-        require("lazyvim.util").deprecate("treesitter.ensure_installed", "treesitter.opts.ensure_installed")
-      end
       require("nvim-treesitter.configs").setup(opts)
 
       local opt = vim.opt

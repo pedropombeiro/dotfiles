a2b3fead69e13c0a483ea21566b388ee4a02bded
Pedro Pombeiro <noreply@pedro.pombei.ro>
2023-03-14 22:40:09 +0100

    vim: Undefine GIT_EDITOR, as it messes up Fork rebase

--- .shellrc/zshrc.d/configs/git.zsh
+++ .shellrc/zshrc.d/configs/git.zsh
@@ -8,5 +8,3 @@ if [[ -n ${HOMEBREW_PREFIX} ]]; then
   PATH="${PATH}:${HOMEBREW_PREFIX}/opt/git/share/git-core/contrib/git-jump"
   export PATH
 fi
-
-export GIT_EDITOR="nvim -c 'Trouble quickfix'" # Open 'git jump' results directly in quickfix window

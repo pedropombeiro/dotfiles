1db10ad6ebd09c14d983cd7245eb7b14293b3c35
Pedro Pombeiro <noreply@pedro.pombei.ro>
2023-03-15 12:06:57 +0100

    vim: Plug Comment.nvim with treesitter

--- .config/nvim/lua/plugins/Comment.lua
+++ .config/nvim/lua/plugins/Comment.lua
@@ -4,5 +4,12 @@
 
 return {
   "numToStr/Comment.nvim",
-  config = true
+  keys = {
+    { "gc", mode = { "n", "v" } },
+    { "gb", mode = { "n", "v" } },
+  },
+  opts = function()
+    local commentstring_avail, commentstring = pcall(require, "ts_context_commentstring.integrations.comment_nvim")
+    return commentstring_avail and commentstring and { pre_hook = commentstring.create_pre_hook() } or {}
+  end,
 }

fa57710888c66a5785e412d822cf985076df66f9
Pedro Pombeiro <noreply@pedro.pombei.ro>
2023-04-07 14:44:01 +0200

    vim: Install neodev plugin

--- .config/nvim/lua/plugins/lsp-setup.lua
+++ .config/nvim/lua/plugins/lsp-setup.lua
@@ -25,163 +25,175 @@ local keys = {
 }
 
 return {
-  "junnplus/lsp-setup.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  keys = keys,
-  dependencies = {
-    "b0o/schemastore.nvim",     -- 🛍  JSON schemas for Neovim
-    {
-      "neovim/nvim-lspconfig",  -- Quickstart configs for Nvim LSP
-      dependencies = {
-        "hrsh7th/cmp-nvim-lsp", -- nvim-cmp source for neovim builtin LSP client
-        {
-          --- uses Mason to ensure installation of user specified LSP servers and will tell nvim-lspconfig what command
-          --- to use to launch those servers.
-          "williamboman/mason-lspconfig.nvim",
-          dependencies = "williamboman/mason.nvim",
+  {
+    "junnplus/lsp-setup.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    keys = keys,
+    dependencies = {
+      "b0o/schemastore.nvim",     -- 🛍  JSON schemas for Neovim
+      {
+        "neovim/nvim-lspconfig",  -- Quickstart configs for Nvim LSP
+        dependencies = {
+          "hrsh7th/cmp-nvim-lsp", -- nvim-cmp source for neovim builtin LSP client
+          {
+            --- uses Mason to ensure installation of user specified LSP servers and will tell nvim-lspconfig what command
+            --- to use to launch those servers.
+            "williamboman/mason-lspconfig.nvim",
+            dependencies = "williamboman/mason.nvim",
+          },
         },
       },
+      {
+        --- Uses Mason to ensure installation of user specified LSP servers and will tell nvim-lspconfig what command
+        --- to use to launch those servers.
+        "williamboman/mason-lspconfig.nvim",
+        dependencies = "williamboman/mason.nvim",
+      }
     },
-    {
-      --- Uses Mason to ensure installation of user specified LSP servers and will tell nvim-lspconfig what command
-      --- to use to launch those servers.
-      "williamboman/mason-lspconfig.nvim",
-      dependencies = "williamboman/mason.nvim",
-    }
-  },
-  init = function()
-    local config = require("config")
+    init = function()
+      local config = require("config")
 
-    local signs = {
-      ---@diagnostic disable: undefined-field
-      Error = config.icons.diagnostics.error .. " ",
-      Warn  = config.icons.diagnostics.warning .. " ",
-      Hint  = config.icons.diagnostics.hint .. " ",
-      Info  = config.icons.diagnostics.info .. " "
-      ---@diagnostic enable: undefined-field
-    }
-    for type, icon in pairs(signs) do
-      local hl = "DiagnosticSign" .. type
-      vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-    end
-
-    local m = require("mapx")
-    m.nname("<leader>l", "LSP")
-    m.nname("<leader>lw", "Workspace")
-  end,
-  config = function()
-    --if !has_key(plugs, "trouble.nvim")
-    --  nnoremap <silent> <leader>gd :lua vim.lsp.buf.definition()<CR>
-    --  nnoremap <silent> <C-]> :lua vim.lsp.buf.definition()<CR>
-    --  nnoremap <silent> <leader>gr :lua vim.lsp.buf.references()<CR>
-    --  nnoremap <silent> <leader>xq :lua vim.diagnostic.setloclist()<CR>
-    --endif
-
-    local function file_exists(name)
-      local f = io.open(name, "r")
-      if f ~= nil then
-        io.close(f)
-        return true
-      else
-        return false
+      local signs = {
+        ---@diagnostic disable: undefined-field
+        Error = config.icons.diagnostics.error .. " ",
+        Warn  = config.icons.diagnostics.warning .. " ",
+        Hint  = config.icons.diagnostics.hint .. " ",
+        Info  = config.icons.diagnostics.info .. " "
+        ---@diagnostic enable: undefined-field
+      }
+      for type, icon in pairs(signs) do
+        local hl = "DiagnosticSign" .. type
+        vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
       end
-    end
 
-    local servers = {
-      bashls = {},
-      dockerls = {},
-      golangci_lint_ls = {},
-      gopls = {},
-      jsonls = {
-        settings = {
-          json = {
-            schemas = require("schemastore").json.schemas(),
-            validate = { enable = true },
+      local m = require("mapx")
+      m.nname("<leader>l", "LSP")
+      m.nname("<leader>lw", "Workspace")
+    end,
+    config = function()
+      --if !has_key(plugs, "trouble.nvim")
+      --  nnoremap <silent> <leader>gd :lua vim.lsp.buf.definition()<CR>
+      --  nnoremap <silent> <C-]> :lua vim.lsp.buf.definition()<CR>
+      --  nnoremap <silent> <leader>gr :lua vim.lsp.buf.references()<CR>
+      --  nnoremap <silent> <leader>xq :lua vim.diagnostic.setloclist()<CR>
+      --endif
+
+      local function file_exists(name)
+        local f = io.open(name, "r")
+        if f ~= nil then
+          io.close(f)
+          return true
+        else
+          return false
+        end
+      end
+
+      local servers = {
+        bashls = {},
+        dockerls = {},
+        golangci_lint_ls = {},
+        gopls = {},
+        jsonls = {
+          settings = {
+            json = {
+              schemas = require("schemastore").json.schemas(),
+              validate = { enable = true },
+            },
           },
         },
-      },
-      marksman = {},
-      lua_ls = {
-        settings = {
-          Lua = {
-            runtime = {
-              -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-              version = "LuaJIT",
-            },
-            diagnostics = {
-              -- Get the language server to recognize the `vim` global
-              globals = { "vim" },
-              neededFileStatus = {
-                ["codestyle-check"] = "Any",
+        marksman = {},
+        lua_ls = {
+          single_file_support = true,
+          settings = {
+            Lua = {
+              runtime = {
+                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+                version = "LuaJIT",
+              },
+              diagnostics = {
+                -- Get the language server to recognize the `vim` global
+                globals = { "vim" },
+                neededFileStatus = {
+                  ["codestyle-check"] = "Any",
+                },
+              },
+              workspace = {
+                -- Make the server aware of Neovim runtime files
+                library = vim.api.nvim_get_runtime_file("", true),
+                checkThirdParty = false,
+              },
+              completion = {
+                callSnippet = "Replace",
+              },
+              format = {
+                enable = true,
+                defaultConfig = {
+                  indent_style = "space",
+                  indent_size = "2",
+                  quote_style = "single",
+                },
               },
             },
-            workspace = {
-              -- Make the server aware of Neovim runtime files
-              library = vim.api.nvim_get_runtime_file("", true),
-              checkThirdParty = false,
-            },
-            completion = {
-              callSnippet = "Replace",
-            },
-            format = {
-              enable = true,
-              defaultConfig = {
-                indent_style = "space",
-                indent_size = "2",
-                quote_style = "double",
+          },
+        },
+        sqlls = {},
+        taplo = {},
+        vimls = {},
+        yamlls = {
+          settings = {
+            yaml = {
+              hover = true,
+              completion = true,
+              validate = true,
+              schemastore = {
+                enable = true,
+                url = "https://www.schemastore.org/api/json/catalog.json",
               },
             },
           },
         },
-      },
-      sqlls = {},
-      taplo = {},
-      vimls = {},
-      yamlls = {
-        settings = {
-          yaml = {
-            hover = true,
-            completion = true,
-            validate = true,
-            schemastore = {
-              enable = true,
-              url = "https://www.schemastore.org/api/json/catalog.json",
-            },
-          },
-        },
-      },
-    }
-    if file_exists(vim.fn.expand("~/Library/Arduino15/arduino-cli.yaml")) then
-      servers.arduino_language_server = {
-        cmd = {
-          "arduino-language-server",
-          "-cli-config", "~/Library/Arduino15/arduino-cli.yaml", -- Generated with `arduino-cli config init`
-          "-fqbn", "keyboardio:gd32:keyboardio_model_100",
-          "-cli", "arduino-cli",
-          "-clangd", "clangd"
-        }
       }
-      servers.clangd = {}
-    end
-    if vim.fn.executable("solargraph") == 1 then
-      servers.solargraph = {
-        flags = {
-          debounce_text_changes = 150,
+      if file_exists(vim.fn.expand("~/Library/Arduino15/arduino-cli.yaml")) then
+        servers.arduino_language_server = {
+          cmd = {
+            "arduino-language-server",
+            "-cli-config", "~/Library/Arduino15/arduino-cli.yaml", -- Generated with `arduino-cli config init`
+            "-fqbn", "keyboardio:gd32:keyboardio_model_100",
+            "-cli", "arduino-cli",
+            "-clangd", "clangd"
+          }
         }
-      }
-    end
+        servers.clangd = {}
+      end
+      if vim.fn.executable("solargraph") == 1 then
+        servers.solargraph = {
+          flags = {
+            debounce_text_changes = 150,
+          }
+        }
+      end
 
-    require("lsp-setup").setup({
-      default_mappings = false,
-      -- Global on_attach
-      on_attach = function(client, _)
-        -- Support custom the on_attach function for global
-        -- Formatting on save as default
-        require("lsp-setup.utils").format_on_save(client)
-      end,
-      -- Global capabilities
-      capabilities = vim.lsp.protocol.make_client_capabilities(),
-      servers = servers,
-    })
-  end
+      require("neodev").setup({
+        -- add any options here, or leave empty to use the default settings
+      })
+
+      require("lsp-setup").setup({
+        default_mappings = false,
+        -- Global on_attach
+        on_attach = function(client, _)
+          -- Support custom the on_attach function for global
+          -- Formatting on save as default
+          require("lsp-setup.utils").format_on_save(client)
+        end,
+        -- Global capabilities
+        capabilities = vim.lsp.protocol.make_client_capabilities(),
+        servers = servers,
+      })
+    end
+  },
+  {
+    -- 💻 Neovim setup for init.lua and plugin development with full signature help, docs and completion for the nvim lua API.
+    "folke/neodev.nvim",
+    opts = { experimental = { pathStrict = true } },
+  }
 }

5d727259b297f0e192a567edf693bdae4aad5a99
Pedro Pombeiro <noreply@pedro.pombei.ro>
2023-04-07 23:16:12 +0200

    vim: Lazy load neodev plugin

--- .config/nvim/lua/plugins/lsp-setup.lua
+++ .config/nvim/lua/plugins/lsp-setup.lua
@@ -194,6 +194,7 @@ return {
   {
     -- 💻 Neovim setup for init.lua and plugin development with full signature help, docs and completion for the nvim lua API.
     'folke/neodev.nvim',
+    lazy = true,
     opts = { experimental = { pathStrict = true } },
   }
 }

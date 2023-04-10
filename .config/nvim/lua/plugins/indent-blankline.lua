e81009f8416145bbfdb34b2926433c0731829ba2
Pedro Pombeiro <noreply@pedro.pombei.ro>
2023-04-10 19:18:12 +0200

    vim: Enhance indent-blankline with mini.indentscope

--- .config/nvim/lua/plugins/indent-blankline.lua
+++ .config/nvim/lua/plugins/indent-blankline.lua
@@ -1,13 +1,43 @@
--- mapx (https://github.com/b0o/mapx.nvim)
---  🗺 A better way to create key mappings in Neovim
+-- indent-blankline.nvim (https://github.com/lukas-reineke/indent-blankline.nvim)
+--  Indent guides for Neovim
+
+local filetype_exclude = { 'help', 'alpha', 'dashboard', 'neo-tree', 'Trouble', 'lazy', 'mason' }
+local symbol = '│'
 
 return {
-  'lukas-reineke/indent-blankline.nvim', -- Indent guides for Neovim
-  event = 'BufReadPost',
-  opts = {
-    char = '│',
-    filetype_exclude = { 'help', 'alpha', 'dashboard', 'neo-tree', 'Trouble', 'lazy' },
-    show_trailing_blankline_indent = false,
-    show_current_context = false,
+  {
+    'lukas-reineke/indent-blankline.nvim', -- Indent guides for Neovim
+    event = { 'BufReadPre', 'BufNewFile' },
+    opts = {
+      char = symbol,
+      filetype_exclude = filetype_exclude,
+      show_trailing_blankline_indent = false,
+      show_current_context = false,
+    },
+  },
+  {
+    'echasnovski/mini.indentscope', -- Visualize and work with indent scope
+    version = '*',
+    event = { 'BufReadPre', 'BufNewFile' },
+    init = function()
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = filetype_exclude,
+        callback = function()
+          vim.b.miniindentscope_disable = true
+        end,
+      })
+    end,
+    opts = {
+      symbol = symbol,
+      options = { try_as_border = true },
+      draw = {
+        animation = function(next, total)
+          return 5
+        end
+      }
+    },
+    config = function(_, opts)
+      require('mini.indentscope').setup(opts)
+    end,
   },
 }

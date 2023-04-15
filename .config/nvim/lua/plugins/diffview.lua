e9225d69b3533c6b805ace42baac73205f446da4
Pedro Pombeiro <noreply@pedro.pombei.ro>
2023-04-15 23:33:30 +0200

    vim: Standardize icons in diffview plugin

--- .config/nvim/lua/plugins/diffview.lua
+++ .config/nvim/lua/plugins/diffview.lua
@@ -6,5 +6,18 @@ return {
   dependencies = 'nvim-lua/plenary.nvim',
   cmd = { 'DiffviewOpen', 'DiffviewClose', 'DiffviewToggleFiles', 'DiffviewFocusFiles' },
   keys = { { '<leader>gd', '<cmd>DiffviewOpen<cr>', desc = 'DiffView' } },
-  config = true
+  opts = function()
+    local config = require('config')
+
+    return {
+      icons = {
+        folder_closed = config.icons.folder.collapsed,
+        folder_open = config.icons.folder.expanded,
+      },
+      signs = {
+        fold_closed = config.icons.expander.collapsed,
+        fold_open = config.icons.expander.expanded,
+      },
+    }
+  end
 }

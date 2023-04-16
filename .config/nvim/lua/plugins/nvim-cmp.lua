36997cf5fdf4e24c0e2000768cf60c4f77ae65ce
Pedro Pombeiro <noreply@pedro.pombei.ro>
2023-04-16 23:27:16 +0200

    vim: Improve nvim-cmp config

--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -5,11 +5,13 @@ return {
   'hrsh7th/nvim-cmp',
   event = 'InsertEnter',
   dependencies = {
-    'hrsh7th/cmp-buffer',   -- nvim-cmp source for buffer words
-    'hrsh7th/cmp-nvim-lsp', -- nvim-cmp source for neovim builtin LSP client
-    'hrsh7th/cmp-path',     -- nvim-cmp source for path
-    'hrsh7th/cmp-vsnip',    -- nvim-cmp source for vim-vsnip
-    'hrsh7th/cmp-cmdline',  -- nvim-cmp source for vim's cmdline
+    'hrsh7th/cmp-buffer',                  -- nvim-cmp source for buffer words
+    'hrsh7th/cmp-nvim-lsp',                -- nvim-cmp source for neovim builtin LSP client
+    'hrsh7th/cmp-nvim-lua',                -- nvim-cmp source for nvim lua
+    'hrsh7th/cmp-path',                    -- nvim-cmp source for path
+    'hrsh7th/cmp-vsnip',                   -- nvim-cmp source for vim-vsnip
+    'hrsh7th/cmp-nvim-lsp-signature-help', -- nvim-cmp source for displaying function signatures with the current parameter emphasized
+    'hrsh7th/cmp-cmdline',                 -- nvim-cmp source for vim's cmdline
     { 'hrsh7th/vim-vsnip',                 lazy = true },
     { 'williamboman/mason-lspconfig.nvim', lazy = true },
     {
@@ -19,16 +21,22 @@ return {
   },
   opts = function()
     local cmp = require('cmp')
+    local kinds = require('cmp.types').lsp.CompletionItemKind
 
     cmp.setup({
-      completion = {
-        completeopt = 'menu,menuone,noinsert',
+      performance = {
+        debounce = 50,
+        throttle = 10,
       },
+      preselect = cmp.PreselectMode.None,
       snippet = {
         -- REQUIRED - you must specify a snippet engine
         expand = function(args)
           vim.fn['vsnip#anonymous'](args.body) -- For `vsnip` users.
         end,
+      },
+      view = {
+        max_height = 20,
       },
       window = {
         -- completion = cmp.config.window.bordered(),
@@ -43,20 +51,55 @@ return {
         ['<C-b>']     = cmp.mapping.scroll_docs(-4),
         ['<C-f>']     = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-e>']     = cmp.mapping.abort(),
-        ['<CR>']      = cmp.mapping.confirm({ select = false }),
-        ['<S-CR>']    = cmp.mapping.confirm({
+        ['<C-e>']     = cmp.mapping({
+          i = cmp.mapping.abort(),
+          c = cmp.mapping.close(),
+        }),
+        ['<CR>']      = cmp.mapping.confirm({
           behavior = cmp.ConfirmBehavior.Replace,
           select = false,
         }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
       }),
       sources = cmp.config.sources({
-        { name = 'nvim_lsp' },
-        { name = 'vsnip' }, -- For vsnip users.
-      }, {
-        { name = 'buffer' },
+        {
+          name = 'nvim_lsp',
+          priority = 80,
+          group_index = 1,
+          entry_filter = function(entry, context)
+            local kind = entry:get_kind()
+            local line = context.cursor_line
+            local col = context.cursor.col
+            local chars_before = line:sub(col - 4, col - 1)
+
+            if chars_before:find('%.') or chars_before:find('%:') then
+              -- If any previous 4 characters contains a "." or ":"
+              return kind == kinds.Method or kind == kinds.Function or kinds.Field
+            elseif line:match('^%s*%w*$') then
+              -- text in the new line
+              return kind == kinds.Function or kind == kinds.Variable
+            end
+            return true
+          end
+        },
+        { name = 'nvim_lua', priority = 80, group_index = 1 },
+        { name = 'vsnip',    priority = 10, group_index = 2 }, -- For vsnip users.
+        { name = 'path',     priority = 40, max_item_count = 10, group_index = 5 },
+        { name = 'git' },
+        {
+          name = 'buffer',
+          priority = 5,
+          keyword_length = 3,
+          max_item_count = 5,
+          group_index = 5,
+          option = {
+            get_bufnrs = function()
+              return vim.api.nvim_list_bufs()
+            end,
+          },
+        },
         { name = 'cmdline' },
         { name = 'path' },
+        { name = 'nvim_lsp_signature_help' },
       }),
       experimental = {
         ghost_text = {

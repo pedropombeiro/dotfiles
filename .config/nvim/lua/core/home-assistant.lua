7f8b559aa9388b3731f7b75e3a260a855b274602
Pedro Pombeiro <noreply@pedro.pombei.ro>
2023-08-27 15:21:02 +0200

    vim: Use yaml filetype for home-assistant files
    This allows usage of file formatting and linting

--- .config/nvim/lua/core/home-assistant.lua
+++ .config/nvim/lua/core/home-assistant.lua
@@ -3,5 +3,5 @@ local ha_augroup = vim.api.nvim_create_augroup('homeassistant', { clear = true }
 vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
   pattern = { '*/config/home-assistant/*.yaml', '*/config/home-assistant/**/*.yaml' },
   group = ha_augroup,
-  command = 'setlocal filetype=home-assistant syntax=yaml foldmethod=indent commentstring=#\\ %s | LspStart yamlls'
+  command = 'setlocal commentstring=#\\ %s'
 })

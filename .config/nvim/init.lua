e6852d41e263dd0f808d4eef55c6ba9b894c24b0
Pedro Pombeiro <noreply@pedro.pombei.ro>
2023-08-30 00:22:45 +0200

    vim: Disable some rtp plugins

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -29,7 +29,34 @@ vim.opt.rtp:prepend(lazypath)
 
 -- Install plugins
 -- https://github.com/folke/lazy.nvim
-require('lazy').setup('plugins')
+require('lazy').setup({
+  spec = {
+    { import = 'plugins' },
+  },
+  defaults = {
+    lazy = false,
+    -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
+    -- have outdated releases, which may break your Neovim install.
+    version = false, -- always use the latest git commit
+    -- version = "*", -- try installing the latest stable version for plugins that support semver
+  },
+  checker = { enabled = true }, -- automatically check for plugin updates
+  performance = {
+    rtp = {
+      -- disable some rtp plugins
+      disabled_plugins = {
+        'gzip',
+        -- "matchit",
+        -- "matchparen",
+        -- "netrwPlugin",
+        'tarPlugin',
+        'tohtml',
+        'tutor',
+        'zipPlugin',
+      },
+    },
+  },
+})
 
 -- Inspiration: https://github.com/skwp/dotfiles/blob/master/vim/settings.vim
 local vimsettings = '~/.config/nvim/lua/core'

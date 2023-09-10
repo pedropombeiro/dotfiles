f55a22fd44231d8387dbf11212f743254d75a0f2
Pedro Pombeiro <noreply@pedro.pombei.ro>
2023-09-10 11:02:20 +0200

    vim: Lazy load firenvim so that it can be updated by plugin manager

--- .config/nvim/lua/plugins/firenvim.lua
+++ .config/nvim/lua/plugins/firenvim.lua
@@ -5,7 +5,7 @@ return {
   'glacambre/firenvim',
   -- Lazy load firenvim
   -- Explanation: https://github.com/folke/lazy.nvim/discussions/463#discussioncomment-4819297
-  cond = not not vim.g.started_by_firenvim,
+  lazy = not vim.g.started_by_firenvim,
   build = function()
     require('lazy').load({ plugins = 'firenvim', wait = true })
     vim.fn['firenvim#install'](0)

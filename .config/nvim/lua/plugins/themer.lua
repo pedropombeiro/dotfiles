e9e02774d645a8da60c09acb5983745a04d3a14c
Pedro Pombeiro <noreply@pedro.pombei.ro>
2023-11-01 23:58:56 +0100

    vim: Fix diff text colors

--- .config/nvim/lua/plugins/themer.lua
+++ .config/nvim/lua/plugins/themer.lua
@@ -16,21 +16,19 @@ return {
     if theme == 'gruvbox-material-dark-soft' then
       -- Some colors in the soft scheme need adjusting, in order to have an acceptable contrast
       local function define_highlights()
-        vim.cmd [[
+        vim.cmd([[
           highlight DiffAdd ctermbg=4 guibg=#32302f guifg=#8ec07c
           highlight DiffChange ctermbg=5 guibg=#012800
           highlight DiffDelete ctermfg=12 ctermbg=6 gui=bold guibg=#3c1f1e guifg=#fb4934
-          highlight DiffText cterm=bold ctermbg=9 gui=bold guibg=#7daea3
-        ]]
+          highlight DiffText cterm=bold ctermbg=166 ctermfg=229 guibg=#d55d0e guifg=#fbf1c7
+        ]])
       end
 
       local augroup = vim.api.nvim_create_augroup('ThemerColorSchemeRefresh', { clear = true })
-      vim.api.nvim_create_autocmd(
-        { 'BufRead', 'BufNewFile' },
-        {
-          callback = define_highlights,
-          group = augroup,
-        })
+      vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile', 'BufWinEnter' }, {
+        callback = define_highlights,
+        group = augroup,
+      })
     end
 
     return {
@@ -46,5 +44,5 @@ return {
         telescope = true,
       },
     }
-  end
+  end,
 }

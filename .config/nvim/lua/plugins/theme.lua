f1f4831a4eaae48f4db091394e9cfab9db1d487e
Pedro Pombeiro <noreply@pedro.pombei.ro>
2023-11-17 23:54:10 +0100

    vim: Speed up startup

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -18,7 +18,7 @@ return {
         vim.api.nvim_set_hl(0, name, attr)
       end
 
-      local function define_highlights()
+      local function initialize_theme()
         -- Change border for float windows (normally grey)
         set_hl('FloatBorder', { ctermfg = 109, fg = '#83a598' })
         set_hl('FloatermBorder', { link = 'FloatBorder' })
@@ -28,27 +28,27 @@ return {
         set_hl('@field', { ctermfg = 167, fg = '#fb4934' })
         set_hl('@constant', { ctermfg = 109, fg = '#83a598' })
         set_hl('@string', { ctermfg = 142, fg = '#b8bb26' })
+
+        vim.g.gruvbox_material_background = 'hard'
+        vim.g.gruvbox_material_better_performance = 1
+        vim.g.gruvbox_material_foreground = 'hard'
+        vim.g.gruvbox_material_diagnostic_text_highlight = 1
+        vim.g.gruvbox_material_diagnostic_virtual_text = 'highlighted'
+        vim.g.gruvbox_material_enable_italic = 1
+        vim.g.gruvbox_material_float_style = 'dim'
+
+        vim.cmd('colorscheme gruvbox-material')
       end
 
       local augroup = vim.api.nvim_create_augroup('ThemerColorSchemeRefresh', { clear = true })
       vim.api.nvim_create_autocmd('UIEnter', {
-        callback = define_highlights,
+        callback = initialize_theme,
         group = augroup,
       })
-
-      vim.g.gruvbox_material_background = 'hard'
-      vim.g.gruvbox_material_better_performance = 1
-      vim.g.gruvbox_material_foreground = 'hard'
-      vim.g.gruvbox_material_diagnostic_text_highlight = 1
-      vim.g.gruvbox_material_diagnostic_virtual_text = 'highlighted'
-      vim.g.gruvbox_material_enable_italic = 1
-      vim.g.gruvbox_material_float_style = 'dim'
 
       if vim.fn.has('termguicolors') then
         vim.opt.termguicolors = true
       end
-
-      vim.cmd('colorscheme gruvbox-material')
     end
   end,
 }

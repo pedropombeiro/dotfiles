223351b5b1e63efddbf07010786382cd970367a2
Pedro Pombeiro <noreply@pedro.pombei.ro>
2023-11-17 22:38:44 +0100

    vim: Add <C-g> keymap to telescope

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -129,6 +129,21 @@ return {
             ['<c-t>'] = function(...)
               return require('trouble.providers.telescope').open_with_trouble(...)
             end,
+            ['<C-g>'] = function(prompt_bufnr)
+              -- Use nvim-window-picker to choose the window by dynamically attaching a function
+              local action_set = require('telescope.actions.set')
+              local action_state = require('telescope.actions.state')
+
+              local picker = action_state.get_current_picker(prompt_bufnr)
+              picker.get_selection_window = function(picker, entry)
+                local picked_window_id = require('window-picker').pick_window() or vim.api.nvim_get_current_win()
+                -- Unbind after using so next instance of the picker acts normally
+                picker.get_selection_window = nil
+                return picked_window_id
+              end
+
+              return action_set.edit(prompt_bufnr, 'edit')
+            end,
           },
         },
       },

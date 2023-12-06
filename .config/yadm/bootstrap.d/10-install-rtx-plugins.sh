cd5f7a2e14b16ded1ce225e0e4fd7bfa87429feb
Pedro Pombeiro <noreply@pedro.pombei.ro>
2023-12-06 09:42:10 +0100

    rtx: Replace deprecated --install-missing

--- .config/yadm/bootstrap.d/10-install-rtx-plugins.sh
+++ .config/yadm/bootstrap.d/10-install-rtx-plugins.sh
@@ -5,7 +5,7 @@ YADM_SCRIPTS=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")/../scripts" &>/dev/null 
 source "${YADM_SCRIPTS}/colors.sh"
 
 printf "${YELLOW}%s${NC}\n" "Installing rtx plugins..."
-(cd ~ && rtx install --install-missing)
+(cd ~ && rtx install --yes)
 
 if [[ $(uname -s) != 'Darwin' ]]; then
   if [[ ! -f ${HOME}/.fzf.zsh ]]; then

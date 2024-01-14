12dae6509d083818e7de2579b228ae2559e7416a
Pedro Pombeiro <noreply@pedro.pombei.ro>
2024-01-14 21:02:57 +0100

    vim: Fix Luasnip error on TAB

--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -79,6 +79,8 @@ return {
             c = cmp.mapping.close(),
           }),
           ['<Tab>'] = cmp.mapping(function(fallback)
+            local luasnip = require('luasnip')
+
             if cmp.visible() then
               cmp.select_next_item()
               -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
@@ -92,6 +94,8 @@ return {
             end
           end, { 'i', 's' }),
           ['<S-Tab>'] = cmp.mapping(function(fallback)
+            local luasnip = require('luasnip')
+
             if cmp.visible() then
               cmp.select_prev_item()
             elseif luasnip.jumpable(-1) then

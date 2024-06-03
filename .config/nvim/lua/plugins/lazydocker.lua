c0f16f6810cbe00eda43867f0d914bfe553832f3
Pedro Pombeiro <noreply@pedro.pombei.ro>
2024-06-03 23:11:56 +0200

    vim: Fix error invoking Lazydocker when no buffer is open

--- .config/nvim/lua/plugins/lazydocker.lua
+++ .config/nvim/lua/plugins/lazydocker.lua
@@ -4,7 +4,19 @@
 return {
   'crnvl96/lazydocker.nvim',
   keys = {
-    { '<leader>td', ':cd %:h | LazyDocker<CR>', desc = 'Open LazyDocker', noremap = true, silent = true },
+    {
+      '<leader>td',
+      function()
+        if vim.api.nvim_buf_get_name(0) ~= '' then -- Check if there's at least one buffer
+          vim.api.nvim_exec2('cd %:h', { output = false }) -- switch to current directory so that Lazydocker filters containers from current stack
+        end
+
+        require('lazydocker').toggle()
+      end,
+      desc = 'Open LazyDocker',
+      noremap = true,
+      silent = true,
+    },
   },
   opts = {},
   dependencies = {

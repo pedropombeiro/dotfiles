4556d13d23aa4ef7bd529cc0201576a6607ade96
Pedro Pombeiro <noreply@pedro.pombei.ro>
2024-06-08 14:15:14 +0200

    mise: Fix mise self-update on qts

--- .shellrc/zshrc.d/configs/pre/mise.zsh
+++ .shellrc/zshrc.d/configs/pre/mise.zsh
@@ -1,15 +1,13 @@
 #!/usr/bin/env zsh
 
 export MISE_GLOBAL_CONFIG_FILE=~/.tool-versions
-if [ -x "${HOME}/.local/share/mise/bin/mise" ]; then
-  export RTX_DISABLE_DIRENV_WARNING=1
+for mise_path in "${HOME}/.local/share/mise/bin/mise" "${HOME}/.local/bin/mise" mise; do
+  if command -v $mise_path >/dev/null; then
+    export RTX_DISABLE_DIRENV_WARNING=1
 
-  eval "$("${HOME}/.local/share/mise/bin/mise" activate zsh)"
-elif command -v mise >/dev/null; then
-  export RTX_DISABLE_DIRENV_WARNING=1
-
-  eval "$(mise activate zsh)"
-fi
+    eval "$(${mise_path} activate zsh)"
+  fi
+done
 
 if [[ -n $MISE_SHELL ]]; then
   if [[ ! -f ~/.config/zsh/site-functions/_mise ]]; then

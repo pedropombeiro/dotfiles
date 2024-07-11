67948c8b8eacc426f1ce3b00d3c3dd827a1458f2
Pedro Pombeiro <noreply@pedro.pombei.ro>
2024-07-11 22:37:17 +0200

    gdk: Remove session_cookie_token_prefix from gdk.yml

--- .config/yadm/scripts/update-work.zsh
+++ .config/yadm/scripts/update-work.zsh
@@ -44,10 +44,6 @@ gitlab:
   rails:
 #     address: 'http://gdk.test:3000'
 #     sherlock: true
-    session_store:
-      cookie_key: _gitlab_session
-      session_cookie_token_prefix: cell_1_
-      unique_cookie_key_postfix: false
 registry:
   enabled: false
   host: registry.test

33b6f52452b932f0dc0cc16e837adb0674cc52a6
Pedro Pombeiro <noreply@pedro.pombei.ro>
2024-07-15 19:44:33 +0200

    vim: Fix error opening file containing line info in argument list

--- .config/nvim/lua/plugins/urlview.lua
+++ .config/nvim/lua/plugins/urlview.lua
@@ -5,8 +5,6 @@ return {
   'axieax/urlview.nvim',
   cmd = 'UrlView',
   init = function()
-    local urlview_augroup = vim.api.nvim_create_augroup('urlview', { clear = true })
-
     local function open_buffer_urlview()
       if vim.fn.expand('%:p') == vim.fn.stdpath('config') .. '/lua/plugins.lua' then
         vim.cmd([[UrlView lazy]])
@@ -15,20 +13,13 @@ return {
       end
     end
 
-    vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
-      pattern = '*',
-      group = urlview_augroup,
-      callback = function(ev)
-        require('which-key').add({
-          '<leader>fu',
-          open_buffer_urlview,
-          icon = '󰖟',
-          silent = true,
-          remap = true,
-          desc = 'List buffer URLs',
-          buffer = ev['buf'],
-        })
-      end,
+    require('which-key').add({
+      '<leader>fu',
+      open_buffer_urlview,
+      icon = '󰖟',
+      silent = true,
+      remap = true,
+      desc = 'List buffer URLs',
     })
   end,
   opts = {

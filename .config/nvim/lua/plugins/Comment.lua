06e6b8eb482edbb637d733903c4ad94c90dc682a
Pedro Pombeiro <noreply@pedro.pombei.ro>
2024-09-29 23:51:54 +0200

    vim: Remove mini-comment in favor of native comments

--- .config/nvim/lua/plugins/Comment.lua
+++
@@ -1,9 +0,0 @@
--- mini-comment (https://github.com/echasnovski/mini.nvim/blob/main/readmes/mini-comment.md)
---   🧠 💪 // Smart and powerful comment plugin for neovim. Supports treesitter, dot repeat, left-right/up-down motions,
---   hooks, and more
-
-return {
-  'echasnovski/mini.comment',
-  event = { 'BufNewFile', 'BufReadPre' },
-  opts = {},
-}

7b0bb5698fb0b2aab8a5e3ceeab9ac8c27f9ea41
Pedro Pombeiro <noreply@pedro.pombei.ro>
2024-11-06 08:23:52 +0100

    yadm: Use HTTPS remote for pull

--- .config/yadm/bootstrap.d/99-set-yadm-remote.sh
+++ .config/yadm/bootstrap.d/99-set-yadm-remote.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
 
 echo "Updating the yadm repo origin URL"
-yadm remote set-url origin "git@github.com:pedropombeiro/dotfiles.git"
+yadm remote set-url origin "https://github.com/pedropombeiro/dotfiles.git"
+yadm remote set-url --push origin "git@github.com:pedropombeiro/dotfiles.git"

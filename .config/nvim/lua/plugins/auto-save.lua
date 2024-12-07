b0aef48d17f7c9f03d325f7f138df1198d86837b
Pedro Pombeiro <noreply@pedro.pombei.ro>
2024-12-07 10:34:52 +0100

    vim: Fix auto-save LSP format errors on ruby files

--- .config/nvim/lua/plugins/auto-save.lua
+++ .config/nvim/lua/plugins/auto-save.lua
@@ -8,5 +8,12 @@ return {
   ft = { 'eruby', 'html' },
   opts = {
     debounce_delay = 500, -- delay after which a pending save is executed
+    trigger_events = {
+      immediate_save = { 'BufLeave', 'FocusLost' }, -- vim events that trigger an immediate save
+      defer_save = {
+        { 'InsertLeave', 'TextChanged', pattern = { '*.erb', '*.css', '*.scss' } },
+      },
+      cancel_deferred_save = { 'InsertEnter' }, -- vim events that cancel a pending deferred save
+    },
   },
 }

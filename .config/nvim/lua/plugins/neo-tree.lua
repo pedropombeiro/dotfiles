e257ff2c46b6cf7d166b8ec953b54837ffb4200d
Pedro Pombeiro <noreply@pedro.pombei.ro>
2025-01-19 23:02:14 +0100

    vim: Load nvim-window-picker lazily

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -15,7 +15,7 @@ return {
     {
       's1n7ax/nvim-window-picker',
       name = 'window-picker',
-      event = 'VeryLazy',
+      lazy = true,
       version = '2.*',
       opts = function()
         local colors = require('config').theme.colors

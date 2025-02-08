dd7dc6bdbe3ea416771b9eda5ec6d10e21bf4afe
Pedro Pombeiro <noreply@pedro.pombei.ro>
2025-02-08 21:53:56 +0100

    vim: Enable LazyDev integration with blink-cmp

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -66,7 +66,15 @@ return {
       -- Default list of enabled providers defined so that you can extend it
       -- elsewhere in your config, without redefining it, due to `opts_extend`
       sources = {
-        default = { 'lsp', 'path', 'snippets', 'buffer' },
+        default = { 'lazydev', 'lsp', 'path', 'snippets', 'buffer' },
+        providers = {
+          lazydev = {
+            name = 'LazyDev',
+            module = 'lazydev.integrations.blink',
+            -- make lazydev completions top priority (see `:h blink.cmp`)
+            score_offset = 100,
+          },
+        },
       },
     },
     opts_extend = { 'sources.default' },

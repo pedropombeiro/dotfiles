a83fc2160963cb44a871865b655f9ecb8cb33e4b
Pedro Pombeiro <noreply@pedro.pombei.ro>
2025-02-09 23:25:52 +0100

    vim: Fix gitsigns not attaching to buffer

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -4,11 +4,9 @@
 local on_attach = function(bufnr)
   local name = vim.api.nvim_buf_get_name(bufnr)
   if vim.fn.expand("%:t") == "lsp.log" or vim.bo.filetype == "help" then return false end
+
   local size = vim.fn.getfsize(name)
   if size > 1024 * 1024 * 5 then return false end
-
-  local is_git_repo = vim.fn.systemlist("git", { "rev-parse", "--is-inside-work-tree" })[1] == "true"
-  if not is_git_repo then return false end
 
   local gs = package.loaded.gitsigns
   local wk = require("which-key")
@@ -26,18 +24,9 @@ local on_attach = function(bufnr)
       { "<leader>hp", gs.preview_hunk_inline, desc = "Preview Git hunk", icon = "" },
       { "<leader>hS", gs.stage_buffer, desc = "Stage buffer" },
       { "<leader>hR", gs.reset_buffer, desc = "Reset buffer" },
-      {
-        "<leader>hb",
-        function() gs.blame_line({ full = true }) end,
-        desc = "Git blame line",
-      },
+      { "<leader>hb", function() gs.blame_line({ full = true }) end, desc = "Git blame line" },
       { "<leader>hd", gs.diffthis, desc = "Git diff this" },
-      {
-        "<leader>hD",
-        function() gs.diffthis("~") end,
-        desc = "Git diff this ~",
-        icon = "",
-      },
+      { "<leader>hD", function() gs.diffthis("~") end, desc = "Git diff this ~", icon = "" },
       {
         expr = true,
         {
@@ -53,20 +42,6 @@ local on_attach = function(bufnr)
             icon = "󰮳",
           },
           {
-            -- Options
-            { "[g", group = "Enable Git option", icon = "" },
-            {
-              "[gb",
-              function() gs.toggle_current_line_blame(true) end,
-              desc = "Enable current line blame",
-            },
-            {
-              "[gd",
-              function() gs.toggle_deleted(true) end,
-              desc = "Enable Git-deleted",
-            },
-          },
-          {
             -- Navigation
             "]c",
             function()
@@ -77,25 +52,25 @@ local on_attach = function(bufnr)
             desc = "Next Git hunk",
             icon = "󰮱",
           },
+          {
+            -- Options
+            { "[g", group = "Enable Git option", icon = "" },
+            { "[gb", function() gs.toggle_current_line_blame(true) end, desc = "Enable current line blame" },
+
+
+            { "[gd", function() gs.toggle_deleted(true) end, desc = "Enable Git-deleted" },
+          },
           -- Options
           { "]g", group = "Disable Git option", icon = "" },
-          {
-            "]gb",
-            function() gs.toggle_current_line_blame(false) end,
-            desc = "Disable current line blame",
-          },
-          {
-            "]gd",
-            function() gs.toggle_deleted(false) end,
-            desc = "Disable Git-deleted",
-          },
+          { "]gb", function() gs.toggle_current_line_blame(false) end, desc = "Disable current line blame" },
+          { "]gd", function() gs.toggle_deleted(false) end, desc = "Disable Git-deleted" },
         },
       },
       {
         -- Text object
         mode = { "o", "x" },
         { "i", group = "Text object" },
-        { "ih", ":<C-U>Gitsigns select_hunk<CR>", desc = "Select Git hunk" },
+        { "ih", function() gs.select_hunk() end, desc = "Select Git hunk" },
       },
     },
   })
@@ -105,6 +80,10 @@ return {
   "lewis6991/gitsigns.nvim",
   dependencies = "nvim-lua/plenary.nvim",
   event = { "BufReadPost", "BufNewFile", "BufWritePre" },
+  cond = function()
+    local is_git_repo = vim.fn.systemlist({ "git", "rev-parse", "--is-inside-work-tree" })[1] == "true"
+    return is_git_repo
+  end,
   opts = {
     signs = {
       add = { text = "▌", show_count = true },
@@ -149,11 +128,10 @@ return {
   },
   config = function(_, opts)
     require("gitsigns").setup(opts)
-
     require("scrollbar.handlers.gitsigns").setup()
 
     -- Compare with the default branch
-    local branch = string.match(vim.fn.system('git branch -rl "*/HEAD"'), ".*/(.*)\n")
+    local branch = string.match(vim.fn.system("git branch -rl '*/HEAD'"), ".*/(.*)\n")
     if branch then require("gitsigns").change_base(branch, true) end
   end,
 }

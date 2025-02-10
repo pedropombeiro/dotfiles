681482d08dfab322b6c318251da147d611da872d
Pedro Pombeiro <noreply@pedro.pombei.ro>
2025-02-10 23:25:31 +0100

    vim: Code cleanup

--- .config/nvim/lua/plugins/lsp/lspconfig.lua
+++ .config/nvim/lua/plugins/lsp/lspconfig.lua
@@ -264,12 +264,12 @@ return {
 
     require("lspconfig.ui.windows").default_options.border = border
 
-    local isBlinkLoaded = package.loaded["blink.cmp"] ~= nil
-    if isBlinkLoaded then
+    local blink_cmp = package.loaded["blink.cmp"]
+    if blink_cmp ~= nil then
       for server, config in pairs(opts.servers) do
         -- passing config.capabilities to blink.cmp merges with the capabilities in your
         -- `opts[server].capabilities, if you've defined it
-        config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
+        config.capabilities = blink_cmp.get_lsp_capabilities(config.capabilities)
         lspconfig[server].setup(config)
       end
     end

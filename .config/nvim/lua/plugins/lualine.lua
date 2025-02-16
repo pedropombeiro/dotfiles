9345cd59fc15ac55a4a4c49371683b46fab82ef3
Pedro Pombeiro <noreply@pedro.pombei.ro>
2025-02-16 22:13:02 +0100

    vim: Fix git branch in lualine for yadm repo

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -2,107 +2,123 @@
 --  A blazing fast and easy to configure neovim statusline plugin written in pure lua.
 
 return {
-  "nvim-lualine/lualine.nvim",
-  event = { "BufNewFile", "BufReadPost" },
-  dependencies = {
-    "kyazdani42/nvim-web-devicons",
-  },
-  init = function()
-    local opt = vim.opt
+  {
+    "nvim-lualine/lualine.nvim",
+    event = { "BufNewFile", "BufReadPost" },
+    dependencies = {
+      "kyazdani42/nvim-web-devicons",
+    },
+    opts = function()
+      ---@type pmsp.neovim.Config
+      local config = require("config")
+      local diagnostic_icons = config.ui.icons.diagnostics
+      local symbol_icons = config.ui.icons.symbols
+      local opt = vim.opt
 
-    opt.showmode = false -- The mode is shown in Lualine anyway
-    opt.showcmd = false -- The selected line count is already shown in Lualine
-    if vim.fn.has("nvim-0.9") == 1 then
-      opt.shortmess:append({ S = true }) -- do not show search count message when searching
-    end
-  end,
-  opts = function()
-    ---@type pmsp.neovim.Config
-    local config = require("config")
-    local diagnostic_icons = config.ui.icons.diagnostics
-    local symbol_icons = config.ui.icons.symbols
-    local sections = {
-      lualine_a = { "mode" },
-      lualine_b = { "branch" },
-      lualine_c = {
-        {
-          "diagnostics",
-          sources = { "nvim_lsp", "nvim_diagnostic" },
-          symbols = {
-            hint = diagnostic_icons.hint .. " ",
-            info = diagnostic_icons.info .. " ",
-            warn = diagnostic_icons.warning .. " ",
-            error = diagnostic_icons.error .. " ",
+      opt.showmode = false -- The mode is shown in Lualine anyway
+      opt.showcmd = false -- The selected line count is already shown in Lualine
+      if vim.fn.has("nvim-0.9") == 1 then
+        opt.shortmess:append({ S = true }) -- do not show search count message when searching
+      end
+
+      local function firenvim_cond() return not vim.g.started_by_firenvim end
+      local function diff_source()
+        local gitsigns = vim.b.gitsigns_status_dict
+        if gitsigns then
+          return {
+            added = gitsigns.added,
+            modified = gitsigns.changed,
+            removed = gitsigns.removed,
+          }
+        end
+      end
+
+      local sections = {
+        lualine_a = { "mode" },
+        lualine_b = {
+          { "b:gitsigns_head", icon = "" },
+          {
+            "diff",
+            cond = firenvim_cond,
+            symbols = {
+              added = symbol_icons.added .. " ",
+              modified = symbol_icons.modified .. " ",
+              removed = symbol_icons.removed .. " ",
+            },
+            source = diff_source,
           },
         },
-        {
+        lualine_c = {
+          {
+            "diagnostics",
+            sources = { "nvim_lsp", "nvim_diagnostic" },
+            symbols = {
+              hint = diagnostic_icons.hint .. " ",
+              info = diagnostic_icons.info .. " ",
+              warn = diagnostic_icons.warning .. " ",
+              error = diagnostic_icons.error .. " ",
+            },
+          },
+          {
+            "filetype",
+            cond = firenvim_cond,
+            icon_only = true,
+            separator = "",
+            padding = { left = 1, right = 0 },
+          },
+          {
+            "filename",
+            cond = firenvim_cond,
+            show_filename_only = false,
+            path = 1,
+            shorting_target = 80,
+            symbols = {
+              modified = symbol_icons.modified,
+              readonly = symbol_icons.readonly, -- Text to show when the file is non-modifiable or readonly.
+            },
+          },
+        },
+        lualine_x = {
+          {
+            require("lazy.status").updates,
+            cond = require("lazy.status").has_updates,
+          },
+          {
+            "pipeline", -- https://github.com/topaxi/pipeline.nvim
+            cond = firenvim_cond,
+          },
+          "encoding",
+          "fileformat",
           "filetype",
-          cond = function() return not vim.g.started_by_firenvim end,
-          icon_only = true,
-          separator = "",
-          padding = { left = 1, right = 0 },
         },
-        {
-          "filename",
-          cond = function() return not vim.g.started_by_firenvim end,
-          show_filename_only = false,
-          path = 1,
-          shorting_target = 80,
-          symbols = {
-            modified = symbol_icons.modified,
-            readonly = symbol_icons.readonly, -- Text to show when the file is non-modifiable or readonly.
-          },
+        lualine_y = {
+          "selectioncount",
+          { "searchcount", maxcount = 999, timeout = 500 },
         },
-      },
-      lualine_x = {
-        {
-          require("lazy.status").updates,
-          cond = require("lazy.status").has_updates,
+        lualine_z = {
+          "progress",
+          "location",
         },
-        {
-          "pipeline", -- https://github.com/topaxi/pipeline.nvim
-          cond = function() return not vim.g.started_by_firenvim end,
-        },
-        {
-          "diff",
-          cond = function() return not vim.g.started_by_firenvim end,
-          symbols = {
-            added = symbol_icons.added .. " ",
-            modified = symbol_icons.modified .. " ",
-            removed = symbol_icons.removed .. " ",
-          },
-        },
-        "encoding",
-        "fileformat",
-        "filetype",
-      },
-      lualine_y = {
-        "selectioncount",
-        { "searchcount", maxcount = 999, timeout = 500 },
-      },
-      lualine_z = {
-        "progress",
-        "location",
-      },
-    }
+      }
 
-    return {
-      options = {
-        icons_enabled = true,
-        ignore_focus = { "neotest-summary", "lazygit", "TelescopePrompt", "Outline" },
-        disabled_filetypes = {
-          statusline = { "snacks_dashboard", "Fm", "mason", "neotest-summary" },
+      return {
+        options = {
+          icons_enabled = true,
+          ignore_focus = { "neotest-summary", "lazygit", "TelescopePrompt", "Outline" },
+          disabled_filetypes = {
+            statusline = { "snacks_dashboard", "Fm", "mason", "neotest-summary" },
+          },
+          globalstatus = true,
+          theme = "gruvbox",
         },
-        globalstatus = true,
-        theme = "gruvbox",
-      },
-      extensions = {
-        "fugitive",
-        "man",
-        "lazy",
-        "neo-tree",
-      },
-      sections = sections,
-    }
-  end,
+        extensions = {
+          "fugitive",
+          "man",
+          "lazy",
+          "neo-tree",
+        },
+        sections = sections,
+      }
+    end,
+  },
 }

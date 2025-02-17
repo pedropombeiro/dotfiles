1949cfbeceeeac22b62b07c41ba816f813613392
Pedro Pombeiro <noreply@pedro.pombei.ro>
2025-02-17 18:01:23 +0100

    vim: Fix typo in firenvim config

--- .config/nvim/lua/plugins/firenvim.lua
+++ .config/nvim/lua/plugins/firenvim.lua
@@ -7,7 +7,7 @@ return {
   -- Explanation: https://github.com/folke/lazy.nvim/discussions/463#discussioncomment-4819297
   lazy = not vim.g.started_by_firenvim,
   build = function()
-    require("lazy").load({ plugins = "firenvim", wait = true })
+    require("lazy").load({ plugins = { "firenvim" }, wait = true })
     vim.fn["firenvim#install"](0)
   end,
   init = function()

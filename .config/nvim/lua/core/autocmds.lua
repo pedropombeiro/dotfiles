fff9aab7737154a76560ba337c6431195ece8a2e
Pedro Pombeiro <noreply@pedro.pombei.ro>
2025-02-22 08:53:02 +0100

    vim: Improve autocmds

--- .config/nvim/lua/core/autocmds.lua
+++ .config/nvim/lua/core/autocmds.lua
@@ -1,24 +1,25 @@
 local function augroup(name) return vim.api.nvim_create_augroup("_" .. name, { clear = true }) end
 
+-- close some filetypes with <q>
 vim.api.nvim_create_autocmd("FileType", {
   group = augroup("close_with_q"),
   pattern = {
     "PlenaryTestPopup",
     "help",
-    "lspinfo",
-    "man",
-    "notify",
-    "qf",
-    "spectre_panel",
     "startuptime",
-    "tsplayground",
     "neotest-output",
-    "checkhealth",
-    "neotest-summary",
-    "neotest-output-panel",
   },
   callback = function(event)
     vim.bo[event.buf].buflisted = false
-    vim.keymap.set("n", "q", "<Cmd>close<CR>", { buffer = event.buf, silent = true })
+    require("which-key").add( ---@type wk.Spec
+      { "q", "<Cmd>close<CR>", buffer = event.buf, mode = { "n" }, silent = true, icon = "󰈆" }
+    )
   end,
+})
+
+-- make it easier to close man-files when opened inline
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup("man_unlisted"),
+  pattern = { "man" },
+  callback = function(event) vim.bo[event.buf].buflisted = false end,
 })

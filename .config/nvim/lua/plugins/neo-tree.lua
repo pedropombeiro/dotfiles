e1e40ea48e7653d35aa8b7d53b00110f5e2bb816
Pedro Pombeiro <noreply@pedro.pombei.ro>
2025-02-22 23:18:10 +0100

    vim: Clean up neo-tree config

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -1,6 +1,10 @@
 -- neo-tree.lua (https://github.com/nvim-neo-tree/neo-tree.nvim)
 --  Neovim plugin to manage the file system and other tree like structures.
 
+---@type pmsp.neovim.Config
+local config = require("config")
+local icons = config.ui.icons
+
 return {
   {
     "nvim-neo-tree/neo-tree.nvim",
@@ -28,15 +32,101 @@ return {
     init = function()
       vim.g.neo_tree_remove_legacy_commands = 1
       if vim.fn.argc() == 1 then
-        local stat = vim.loop.fs_stat(vim.fn.argv(0))
+        local stat = vim.uv.fs_stat(vim.fn.argv(0))
         if stat and stat.type == "directory" then require("neo-tree") end
       end
     end,
-    opts = function(_, opts)
-      ---@type pmsp.neovim.Config
-      local config = require("config")
-      local icons = config.ui.icons
-
+    opts = {
+      popup_border_style = config.ui.border,
+      default_component_configs = {
+        icon = {
+          folder_empty = icons.folder.empty,
+          folder_empty_open = icons.folder.empty_open,
+          folder_closed = icons.folder.collapsed,
+          folder_open = icons.folder.expanded,
+          default = "",
+        },
+        indent = {
+          expander_collapsed = icons.expander.collapsed,
+          expander_expanded = icons.expander.expanded,
+          expander_highlight = "NeoTreeExpander",
+        },
+        modified = {
+          symbol = icons.symbols.modified,
+        },
+        git_status = {
+          symbols = {
+            -- Change type
+            deleted = icons.symbols.removed, -- this can only be used in the git_status source
+            renamed = icons.symbols.renamed, -- this can only be used in the git_status source
+            -- Status type
+            staged = icons.symbols.staged,
+            unstaged = icons.symbols.unstaged,
+          },
+        },
+      },
+      document_symbols = {
+        kinds = {
+          File = { icon = "󰈙", hl = "Tag" },
+          Namespace = { icon = icons.kinds.Namespace, hl = "Include" },
+          Package = { icon = icons.kinds.Package, hl = "Label" },
+          Class = { icon = icons.kinds.Class, hl = "Include" },
+          Property = { icon = icons.kinds.Property, hl = "@property" },
+          Enum = { icon = icons.kinds.Enum, hl = "@number" },
+          Function = { icon = icons.kinds.Function, hl = "Function" },
+          String = { icon = icons.kinds.String, hl = "String" },
+          Number = { icon = icons.kinds.Number, hl = "Number" },
+          Array = { icon = icons.kinds.Array, hl = "Type" },
+          Object = { icon = icons.kinds.Object, hl = "Type" },
+          Key = { icon = icons.kinds.Key, hl = "" },
+          Struct = { icon = icons.kinds.Struct, hl = "Type" },
+          Operator = { icon = icons.kinds.Operator, hl = "Operator" },
+          TypeParameter = { icon = icons.kinds.TypeParameter, hl = "Type" },
+          StaticMethod = { icon = "󰠄 ", hl = "Function" },
+        },
+      },
+      sources = { "filesystem", "buffers", "git_status", "document_symbols" },
+      open_files_do_not_replace_types = { "terminal", "Trouble", "qf", "Outline", "trouble" },
+      filesystem = {
+        bind_to_cwd = false,
+        follow_current_file = { enabled = true },
+        use_libuv_file_watcher = true,
+        filtered_items = {
+          hide_dotfiles = false,
+          hide_gitignored = true,
+          hide_hidden = true, -- only works on Windows for hidden files/directories
+          hide_by_name = {
+            ".git",
+            "node_modules",
+          },
+          hide_by_pattern = { -- uses glob style patterns
+            "*.zwc",
+          },
+          never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
+            ".DS_Store",
+            "thumbs.db",
+          },
+        },
+        find_args = {
+          fd = {
+            "-uu",
+            "--exclude",
+            ".git",
+            "--exclude",
+            "node_modules",
+            "--exclude",
+            "target",
+          },
+        },
+      },
+      window = {
+        position = "right",
+        mappings = {
+          ["<space>"] = "none",
+        },
+      },
+    },
+    config = function(_, opts)
       vim.fn.sign_define("DiagnosticSignError", { text = icons.diagnostics.error, texthl = "DiagnosticSignError" })
       vim.fn.sign_define("DiagnosticSignWarn", { text = icons.diagnostics.warning, texthl = "DiagnosticSignWarn" })
       vim.fn.sign_define("DiagnosticSignInfo", { text = icons.diagnostics.info, texthl = "DiagnosticSignInfo" })
@@ -52,96 +142,6 @@ return {
       })
       -- -Snacks
 
-      return {
-        default_component_configs = {
-          icon = {
-            folder_empty = icons.folder.empty,
-            folder_empty_open = icons.folder.empty_open,
-            folder_closed = icons.folder.collapsed,
-            folder_open = icons.folder.expanded,
-            default = "",
-          },
-          indent = {
-            expander_collapsed = icons.expander.collapsed,
-            expander_expanded = icons.expander.expanded,
-            expander_highlight = "NeoTreeExpander",
-          },
-          modified = {
-            symbol = icons.symbols.modified,
-          },
-          git_status = {
-            symbols = {
-              -- Change type
-              deleted = icons.symbols.removed, -- this can only be used in the git_status source
-              renamed = icons.symbols.renamed, -- this can only be used in the git_status source
-              -- Status type
-              unstaged = icons.symbols.unstaged,
-            },
-          },
-        },
-        document_symbols = {
-          kinds = {
-            File = { icon = "󰈙", hl = "Tag" },
-            Namespace = { icon = "󰌗", hl = "Include" },
-            Package = { icon = "󰏖", hl = "Label" },
-            Class = { icon = "󰌗", hl = "Include" },
-            Property = { icon = "󰆧", hl = "@property" },
-            Enum = { icon = "󰒻", hl = "@number" },
-            Function = { icon = "󰊕", hl = "Function" },
-            String = { icon = "󰀬", hl = "String" },
-            Number = { icon = "󰎠", hl = "Number" },
-            Array = { icon = "󰅪", hl = "Type" },
-            Object = { icon = "󰅩", hl = "Type" },
-            Key = { icon = "󰌋", hl = "" },
-            Struct = { icon = "󰌗", hl = "Type" },
-            Operator = { icon = "󰆕", hl = "Operator" },
-            TypeParameter = { icon = "󰊄", hl = "Type" },
-            StaticMethod = { icon = "󰠄 ", hl = "Function" },
-          },
-        },
-        sources = { "filesystem", "buffers", "git_status", "document_symbols" },
-        open_files_do_not_replace_types = { "terminal", "Trouble", "qf", "Outline", "trouble" },
-        filesystem = {
-          bind_to_cwd = false,
-          follow_current_file = { enabled = true },
-          use_libuv_file_watcher = true,
-          filtered_items = {
-            hide_dotfiles = false,
-            hide_gitignored = true,
-            hide_hidden = true, -- only works on Windows for hidden files/directories
-            hide_by_name = {
-              ".git",
-              "node_modules",
-            },
-            hide_by_pattern = { -- uses glob style patterns
-              "*.zwc",
-            },
-            never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
-              ".DS_Store",
-              "thumbs.db",
-            },
-          },
-          find_args = {
-            fd = {
-              "-uu",
-              "--exclude",
-              ".git",
-              "--exclude",
-              "node_modules",
-              "--exclude",
-              "target",
-            },
-          },
-        },
-        window = {
-          position = "right",
-          mappings = {
-            ["<space>"] = "none",
-          },
-        },
-      }
-    end,
-    config = function(_, opts)
       require("neo-tree").setup(opts)
 
       -- refresh neotree git status when closing a lazygit terminal
@@ -195,5 +195,5 @@ return {
         },
       }
     end,
-  }
+  },
 }

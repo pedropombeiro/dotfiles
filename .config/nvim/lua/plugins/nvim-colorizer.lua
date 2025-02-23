0984507620013d8dbf1f6cd3a8674992b6990b94
Pedro Pombeiro <noreply@pedro.pombei.ro>
2025-02-23 19:57:30 +0100

    vim: Allow showing color previews on popups

--- .config/nvim/lua/plugins/nvim-colorizer.lua
+++ .config/nvim/lua/plugins/nvim-colorizer.lua
@@ -5,19 +5,11 @@ return {
   "catgoose/nvim-colorizer.lua",
   event = "BufReadPre",
   opts = {
-    filetypes = { "*", "!lazy" },
-    user_default_options = {
-      RGB = true,          -- #RGB hex codes
-      RRGGBB = true,       -- #RRGGBB hex codes
-      names = true,        -- "Name" codes like Blue or blue
-      RRGGBBAA = false,    -- #RRGGBBAA hex codes
-      AARRGGBB = false,    -- 0xAARRGGBB hex codes
-      rgb_fn = false,      -- CSS rgb() and rgba() functions
-      hsl_fn = false,      -- CSS hsl() and hsla() functions
-      css = false,         -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-      css_fn = false,      -- Enable all CSS *functions*: rgb_fn, hsl_fn
-      -- Available modes for `mode`: foreground, background,  virtualtext
-      mode = "background", -- Set the display mode.
+    filetypes = {
+      "*",
+      "!lazy",
+      cmp_docs = { always_update = true },
+      markdown = { always_update = true },
     },
     -- all the sub-options of filetypes apply to buftypes
     buftypes = {
@@ -31,7 +23,6 @@ return {
       "!snacks_dashboard",
       "!lazy",
       "!neotest-summary",
-      "!nofile",
       "!popup",
       "!prompt",
     },

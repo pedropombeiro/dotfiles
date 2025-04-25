28cf88a1e060b0ef2505e015dd081bf033ce7d05
Pedro Pombeiro <noreply@pedro.pombei.ro>
2025-04-26 00:01:09 +0200

    fix(vim): Fix LazyDocker keymap

--- .config/nvim/lua/plugins/lazydocker.lua
+++ .config/nvim/lua/plugins/lazydocker.lua
@@ -2,33 +2,23 @@
 --  Plugin to open LazyDocker without quiting neovim.
 
 return {
-  {
-    "folke/which-key.nvim",
-    lazy = true,
-    opts = {
-      ---@module "which-key"
-      ---@type wk.Spec
-      spec = {
-        {
-          "<leader>td",
-          function()
-            if vim.api.nvim_buf_get_name(0) ~= "" then -- Check if there's at least one buffer
-              vim.api.nvim_exec2("cd %:h", { output = false }) -- switch to current directory so that Lazydocker filters containers from current stack
-            end
+  "crnvl96/lazydocker.nvim",
+  lazy = true,
+  dependencies = { "MunifTanjim/nui.nvim" },
+  ---@module "which-key"
+  ---@type wk.Spec
+  keys = {
+    {
+      "<leader>td",
+      function()
+        if vim.api.nvim_buf_get_name(0) ~= "" then -- Check if there's at least one buffer
+          vim.api.nvim_exec2("cd %:h", { output = false }) -- switch to current directory so that Lazydocker filters containers from current stack
+        end
 
-            require("lazydocker").toggle()
-          end,
-          desc = "Open LazyDocker",
-          icon = "",
-        },
-      },
+        require("lazydocker").toggle()
+      end,
+      desc = "Open LazyDocker",
     },
   },
-
-  {
-    "crnvl96/lazydocker.nvim",
-    lazy = true,
-    dependencies = { "MunifTanjim/nui.nvim" },
-    opts = {},
-  },
+  opts = {},
 }

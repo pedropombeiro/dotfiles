255bf72c9c68e7524f4f85eb4f8794b20ca431fc
Pedro Pombeiro <noreply@pedro.pombei.ro>
2025-05-05 19:44:00 +0200

    refactor(vim): Disable markdown-preview.nvim if running on SSH

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -208,6 +208,7 @@ return {
     "iamcco/markdown-preview.nvim", -- markdown preview plugin for (neo)vim
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
     ft = "markdown",
+    cond = function() return vim.env.SSH_CONNECTION == nil end,
     build = function() vim.fn["mkdp#util#install"]() end,
     init = function() vim.g.mkdp_filetypes = { "markdown" } end,
   },

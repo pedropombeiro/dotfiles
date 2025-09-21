d6ae4fd266268e0a764531ae84eb10581cee61a1
Pedro Pombeiro <noreply@pedro.pombei.ro>
2025-09-21 15:14:51 +0200

    fix(vim): Fix markdown-preview.nvim config

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -246,7 +246,6 @@ return {
     ft = "markdown",
     cond = function() return vim.env.SSH_CONNECTION == nil end,
     build = function() vim.fn["mkdp#util#install"]() end,
-    init = function() vim.g.mkdp_filetypes = { "markdown" } end,
   },
 
   { "NoahTheDuke/vim-just", event = { "BufReadPre", "BufNewFile" }, ft = "just" },

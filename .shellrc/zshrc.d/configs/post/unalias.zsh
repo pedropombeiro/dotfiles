fcaa8023352931031861e111fff3eaef925efd91
Pedro Pombeiro <ppombeiro@gitlab.com>
2025-12-10 16:34:23 +0100

    fix(zsh): fix loading of code that removes problematic aliases
    Broken during zsh-defer optimizations

--- .shellrc/zshrc.d/configs/post/unalias.zsh
+++ .shellrc/zshrc.d/configs/post/unalias.zsh
@@ -1,17 +1,19 @@
 #!/usr/bin/env zsh
 
-# Remove problematic aliases
-if alias duf >/dev/null; then
-  unalias duf
-fi
-if alias t >/dev/null; then
-  # Allow ~/.shellrc/zshrc.d/functions/t to override the t alias
-  unalias t
-fi
+zsh-defer -c "
+  # Remove problematic aliases
+  if alias duf >/dev/null; then
+    unalias duf
+  fi
+  if alias t >/dev/null; then
+    # Allow ~/.shellrc/zshrc.d/functions/t to override the t alias
+    unalias t
+  fi
 
-if alias gf >/dev/null; then
-  unalias gf # Overriden as a more specialized function
-fi
-if alias gfa >/dev/null; then
-  unalias gfa # Overriden as a more specialized function
-fi
+  if alias gf >/dev/null; then
+    unalias gf # Overriden as a more specialized function
+  fi
+  if alias gfa >/dev/null; then
+    unalias gfa # Overriden as a more specialized function
+  fi
+"

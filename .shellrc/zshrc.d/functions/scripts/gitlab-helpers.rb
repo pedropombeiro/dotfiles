797466a8a0f84c5ab8942a3824b60004347a66ec
Pedro Pombeiro <ppombeiro@gitlab.com>
2026-01-06 14:39:24 +0100

    chore(gdk): update gpsup to use ~devops::verify label

--- .shellrc/zshrc.d/functions/scripts/gitlab-helpers.rb
+++ .shellrc/zshrc.d/functions/scripts/gitlab-helpers.rb
@@ -489,7 +489,7 @@ def gpsup(remote, issue_iid)
     "assign='#{ENV.fetch('USER', nil)}'",
     "label='Category:Fleet Visibility'",
     "label='section::ci'",
-    "label='stage::Verify'",
+    "label='devops::verify'",
     "label='group::ci platform'"
   ] + (labels&.map { |label| "label='#{label}'" } || [])
   options << "milestone='#{milestone}'" if milestone

1dd5477f123b6338627321138c29f34dbb9f7f64
Pedro Pombeiro <ppombeiro@gitlab.com>
2026-01-18 00:28:45 +0100

    fix(zsh): align history settings with oh-my-zsh defaults
    - Increase SAVEHIST to 10000
    - Add HIST_EXPIRE_DUPS_FIRST, HIST_VERIFY options

--- .shellrc/zshrc.d/configs/history.zsh
+++ .shellrc/zshrc.d/configs/history.zsh
@@ -2,13 +2,14 @@
 
 HISTFILE="${HISTFILE:-$HOME/.zsh_history}"
 HISTSIZE=50000
-SAVEHIST=4000
+SAVEHIST=10000
 HISTORY_IGNORE='(\/1PE)'
 
-setopt INC_APPEND_HISTORY # Write to the history file immediately, not when the shell exits.
-setopt SHARE_HISTORY      # Share history between all sessions.
-setopt HIST_IGNORE_DUPS   # Don't record an entry that was just recorded again.
-setopt HIST_FIND_NO_DUPS  # Do not display a line previously found.
-setopt sharehistory
-setopt extendedhistory
-setopt histignorespace
+setopt EXTENDED_HISTORY       # Record timestamp of command in HISTFILE
+setopt HIST_EXPIRE_DUPS_FIRST # Delete duplicates first when HISTFILE size exceeds HISTSIZE
+setopt HIST_IGNORE_DUPS       # Ignore duplicated commands history list
+setopt HIST_IGNORE_SPACE      # Ignore commands that start with space
+setopt HIST_FIND_NO_DUPS      # Do not display a line previously found
+setopt HIST_VERIFY            # Show command with history expansion to user before running it
+setopt INC_APPEND_HISTORY     # Write to the history file immediately, not when the shell exits
+setopt SHARE_HISTORY          # Share command history data

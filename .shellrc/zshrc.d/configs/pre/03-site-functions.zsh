f18402c292f782cddc6aa263622349ea560bbf8e
Pedro Pombeiro <ppombeiro@gitlab.com>
2026-01-18 22:57:01 +0100

    fix(zsh): add Homebrew site-functions to fpath for completions

--- .shellrc/zshrc.d/configs/pre/03-site-functions.zsh
+++ .shellrc/zshrc.d/configs/pre/03-site-functions.zsh
@@ -1,7 +1,11 @@
 #!/usr/bin/env zsh
 
 mkdir -p $HOME/.config/zsh/site-functions
-fpath+=($HOME/.config/zsh/site-functions)
+fpath=(
+  ${HOMEBREW_PREFIX:+$HOMEBREW_PREFIX/share/zsh/site-functions}
+  $HOME/.config/zsh/site-functions
+  $fpath
+)
 
 # Recreate any compdump older than a day (using zsh glob instead of slow find)
 # (N) = nullglob, (.) = regular files, (md+1) = modified more than 1 day ago

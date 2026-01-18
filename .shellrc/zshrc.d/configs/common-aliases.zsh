a704a7703bfeb700f471902fced7321b10049997
Pedro Pombeiro <ppombeiro@gitlab.com>
2026-01-18 17:05:07 +0100

    refactor(zsh): move t unalias to common-aliases atload hook
    Remove unalias.zsh as duf is no longer aliased and t unalias
    is now handled in the plugin's atload hook.

--- .shellrc/zshrc.d/configs/common-aliases.zsh
+++ .shellrc/zshrc.d/configs/common-aliases.zsh
@@ -2,5 +2,5 @@
 
 # Load common-aliases via zinit with turbo mode
 # Re-source custom aliases after plugin loads to override plugin defaults (e.g., `la`)
-zinit ice wait'0b' lucid atload'source ~/.shellrc/rc.d/aliases.sh'
+zinit ice wait'0b' lucid atload'unalias t 2>/dev/null; source ~/.shellrc/rc.d/aliases.sh'
 zinit snippet OMZP::common-aliases
--- .shellrc/zshrc.d/configs/post/unalias.zsh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env zsh
-
-# Remove problematic aliases
-unalias duf 2>/dev/null
-unalias t 2>/dev/null # Allow ~/.shellrc/zshrc.d/functions/t to override the t alias

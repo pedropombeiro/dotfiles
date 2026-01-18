babafcf9ffd52cac3d1b3ebbb960dbb9ad97959e
Pedro Pombeiro <ppombeiro@gitlab.com>
2026-01-18 17:02:51 +0100

    fix(zsh): unalias git aliases after zinit turbo loads plugin
    Move gf/gfa/gpsup/gswm unaliases to atload hook so they run after
    the git plugin loads via turbo mode, allowing autoload functions
    to take precedence.

--- .shellrc/zshrc.d/configs/post/unalias.zsh
+++ .shellrc/zshrc.d/configs/post/unalias.zsh
@@ -3,6 +3,3 @@
 # Remove problematic aliases
 unalias duf 2>/dev/null
 unalias t 2>/dev/null # Allow ~/.shellrc/zshrc.d/functions/t to override the t alias
-
-unalias gf 2>/dev/null # Overriden as a more specialized function
-unalias gfa 2>/dev/null # Overriden as a more specialized function

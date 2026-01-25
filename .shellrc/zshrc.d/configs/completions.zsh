39b313da91ccc6936986b6f6f66e7a0a0325b6bd
Pedro Pombeiro <ppombeiro@gitlab.com>
2026-01-25 15:58:38 +0100

    refactor(zsh): consolidate completion generators into single file
    Add opencode completion and move just completion into a unified
    completions.zsh file with a helper function for cleaner maintenance.

---
+++ .shellrc/zshrc.d/configs/completions.zsh
@@ -0,0 +1,17 @@
+#!/usr/bin/env zsh
+
+_generate_completion() {
+  local name=$1
+  local cmd=$2
+  local target=~/.config/zsh/site-functions/_${name}
+
+  if [[ ! -f $target ]]; then
+    eval "$cmd" >"$target"
+    rm -f ~/.zcompdump* >/dev/null 2>&1
+  fi
+}
+
+_generate_completion just 'mise exec just --command "just --completions zsh"'
+_generate_completion opencode 'opencode completion'
+
+unfunction _generate_completion
--- .shellrc/zshrc.d/configs/just.zsh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env zsh
-
-if [[ ! -f ~/.config/zsh/site-functions/_just ]]; then
-  mise exec just --command 'just --completions zsh' >~/.config/zsh/site-functions/_just
-  rm -f '~/.zcompdump*' >/dev/null 2>&1
-fi

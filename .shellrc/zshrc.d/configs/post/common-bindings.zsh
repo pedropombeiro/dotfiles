bcd4f62c74fb2b45f461f0c09f7204c2b5872e2c
Pedro Pombeiro <noreply@pedro.pombei.ro>
2026-02-07 23:59:33 +0100

    feat(zsh): add Ctrl+Arrow word movement, use oc -c for Ctrl+O

--- .shellrc/zshrc.d/configs/post/common-bindings.zsh
+++ .shellrc/zshrc.d/configs/post/common-bindings.zsh
@@ -1,5 +1,12 @@
 #!/usr/bin/env zsh
 
+# Ctrl+Arrow: Word movement (in addition to Alt+Arrow)
+# Escape sequences: ^[[1;5C (Ctrl+Right), ^[[1;5D (Ctrl+Left)
+for keymap in viins vicmd emacs; do
+  bindkey -M $keymap '^[[1;5C' forward-word
+  bindkey -M $keymap '^[[1;5D' backward-word
+done
+
 # Ctrl-O: Launch opencode (replaces current line and executes)
 # Requires unsetting terminal's discard character which normally uses ^O
 if [[ -t 0 ]]; then
@@ -8,7 +15,7 @@ fi
 
 opencode-widget() {
   zle kill-whole-line
-  BUFFER="opencode"
+  BUFFER="oc -c"
   zle accept-line
 }
 zle -N opencode-widget

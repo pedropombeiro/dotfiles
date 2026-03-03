50e31e39ac0e437bf1d0c79627797f3c3e0eb68d
Pedro Pombeiro <ppombeiro@gitlab.com>
2026-03-03 18:29:03 +0100

    feat(shell): add Ctrl-L keybinding to launch lazygit in zoomed tmux pane

--- .shellrc/zshrc.d/configs/post/common-bindings.zsh
+++ .shellrc/zshrc.d/configs/post/common-bindings.zsh
@@ -28,3 +28,27 @@ zle -N opencode-widget
 for keymap in viins vicmd; do
   bindkey -M $keymap '^O' opencode-widget
 done
+
+# Ctrl-L: Launch lazygit in a new tmux window (falls back to replacing current line)
+lazygit-widget() {
+  local cmd
+  if [[ "$PWD" == "$HOME" ]]; then
+    cmd="y lazygit --use-config-dir ~/.config/lazygit"
+  else
+    cmd="lazygit --use-config-dir ~/.config/lazygit"
+  fi
+
+  if [[ -n ${TMUX:-} ]] && command -v tmux >/dev/null; then
+    tmux split-window -Z -c "#{pane_current_path}" "zsh -ilc '$cmd'"
+    zle reset-prompt
+    return
+  fi
+
+  zle kill-whole-line
+  BUFFER="$cmd"
+  zle accept-line
+}
+zle -N lazygit-widget
+for keymap in viins vicmd; do
+  bindkey -M $keymap '^L' lazygit-widget
+done

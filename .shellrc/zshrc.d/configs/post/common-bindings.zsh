5cf4022671800e8e394342e3316187856e4d1ea0
Pedro Pombeiro <noreply@pedro.pombei.ro>
2026-03-05 13:44:21 +0100

    fix(zsh): guard stty call for systems without coreutils

--- .shellrc/zshrc.d/configs/post/common-bindings.zsh
+++ .shellrc/zshrc.d/configs/post/common-bindings.zsh
@@ -2,7 +2,7 @@
 
 # Unset terminal special characters that conflict with app keybindings
 # C-\: SIGQUIT (conflicts with Neovim file explorer toggle)
-if [[ -t 0 ]]; then
+if [[ -t 0 ]] && command -v stty &>/dev/null; then
   stty quit undef
 fi
 

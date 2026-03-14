aa5b4718af52e8c7f0cfb4cf3daf83145798f0ea
Pedro Pombeiro <noreply@pedro.pombei.ro>
2026-03-15 00:43:26 +0100

    perf(zsh): move instant prompt before heavy init
    Reorder pre/ files so p10k instant prompt fires right after brew+grc
    (which needs a working tty), before zinit/compinit/mise/completions.
    This makes the prompt appear in ~10-20ms while the remaining ~500ms
    of initialization runs silently behind the instant prompt redirect.
    
    - 200-preload-grc.zsh -> 011-preload-grc.zsh
    - 300-enable-instant-prompt.zsh -> 012-enable-instant-prompt.zsh

--- .shellrc/zshrc.d/configs/pre/011-preload-grc.zsh
+++ .shellrc/zshrc.d/configs/pre/011-preload-grc.zsh
@@ -1,7 +1,8 @@
 #!/usr/bin/env zsh
 
 # Powerlevel10k Instant Prompt prevents grc.zsh from having a working tty,
-# so we need to make sure we source grc before kicking off Instant Prompt.
+# so we need to make sure we source grc before kicking off Instant Prompt
+# (012-enable-instant-prompt.zsh). Requires HOMEBREW_PREFIX from 010-brew.sh.
 if [[ -s "${HOMEBREW_PREFIX}/etc/grc.zsh" ]]; then
   source "${HOMEBREW_PREFIX}/etc/grc.zsh"
 elif [[ -s /etc/grc.zsh ]]; then

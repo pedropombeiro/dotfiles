75efd138f327a6a3206f85718aebb4ed02be4b4b
Pedro Pombeiro <ppombeiro@gitlab.com>
2026-09-07 10:11:58 +0200

    fix(yadm): update global skills non-interactively

--- .config/yadm/scripts/update-common.zsh
+++ .config/yadm/scripts/update-common.zsh
@@ -177,7 +177,7 @@ fi
 
 _update_step "opencode skills"
 printf "${YELLOW}%s${NC}\n" "Updating OpenCode skills..."
-(( $+commands[npx] )) && npx skills update
+(( $+commands[npx] )) && npx skills update --global --yes
 
 _update_step "skill repositories"
 printf "${YELLOW}%s${NC}\n" "Updating skill repositories..."
